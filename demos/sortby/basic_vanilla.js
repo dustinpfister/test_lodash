cc3844e49b19ee149c2b56093ba5fdf33215b720
dustinpfister <dustin.pfister@gmail.com>
2018-07-06 21:29:05 -0400

    _.sortBy, and array.sort basic examples have matching output

--- demos/sortby/basic_vanilla.js
+++ demos/sortby/basic_vanilla.js
@@ -2,13 +2,19 @@ let _ = require('lodash');
 
 let nums = [5, 42, -5, 7, 6, 3, 52, 27, 158, -1];
 
-console.log(_.sortBy(nums));
+//console.log(nums.sort(function (a, b) {
+
+//        return a < b ? -1 : 1;
+
+//    }));
 
 //[ -5, -1, 3, 5, 6, 7, 27, 42, 52, 158 ]
 
 
-let lessThanTen = _.sortBy(nums, function (n) {
-  return n < 10;
+let lessThanTen = nums.sort(function (a,b) {
+
+return  a < 10 && b > 10 ? 1: -1;
+
 });
 
 console.log(lessThanTen);

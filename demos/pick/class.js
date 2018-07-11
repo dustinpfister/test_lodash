8b7ac396f200915bc84253dc51d9f3e406b71b54
dustinpfister <dustin.pfister@gmail.com>
2018-07-11 12:20:28 -0400

    class demo

--- demos/pick/class.js
+++ demos/pick/class.js
@@ -64,40 +64,15 @@ let day = new Day({
 // my bound rate works
 console.log(day.bounceRate());
 
+console.log( day.constructor.name ); // day
+
 let custom = _.pick(day, ['date', 'pages']);
 
-//custom = new Day(custom);
+console.log( custom.constructor.name ); // Object
+
+custom = new Day(custom);
+
+console.log( custom.constructor.name ); // Day
 
 console.log(custom.bounceRate());
 
-/*
-let day = {
-date: '1/2/17',
-users: 10,
-pageviews: 13,
-pages: [{
-path: '/2017/01/01/hello/',
-users: 3,
-pageviews: 4
-}, {
-path: '/2017/01/02/hello-again/',
-users: 7,
-pgeviews: 9
-}
-
-]
-};
-
-let custom = _.pick(day, ['date','users','pages']);
-
-// what is returned is a shallow clone
-day.users += 50;
-console.log(day.users); // 53
-console.log(custom.users); // 53
-
-// but not a deep clone
-day.pages[0].users += 50;
-console.log(day.pages[0].users); // 53
-console.log(custom.pages[0].users); // 53
-
-*/

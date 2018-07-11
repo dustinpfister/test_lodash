ed38ed3eac1d7dfc7b2299886e818c9081961b65
dustinpfister <dustin.pfister@gmail.com>
2018-07-11 11:47:37 -0400

    deep clone

---
+++ demos/pick/ref.js
@@ -0,0 +1,26 @@
+let _ = require('lodash');
+
+let day = {
+    date: '1/2/17',
+    users: 10,
+    pageviews: 13,
+    pages: [{
+            path: '/2017/01/01/hello/',
+            users: 3,
+            pageviews: 4
+        }, {
+            path: '/2017/01/02/hello-again/',
+            users: 7,
+            pgeviews: 9
+        }
+
+    ]
+};
+
+let custom = _.pick(day, ['date','pages']);
+
+day.pages[0].users += 50;
+
+console.log(day.pages[0].users); // 53
+console.log(custom.pages[0].users); // 53
+

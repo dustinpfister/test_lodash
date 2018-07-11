1431b6d290a38ca22dcc1c6b0aab8954cb063ced
dustinpfister <dustin.pfister@gmail.com>
2018-07-11 10:00:04 -0400

    pick demo

---
+++ demos/pick/basic.js
@@ -0,0 +1,20 @@
+let _ = require('lodash');
+
+var day = {
+    date: '1/2/17',
+    users: 10,
+    pageviews: 13,
+    pages: [{
+            path: '/2017/01/01/hello/',
+            users: 3,
+            pageviews: 4
+        }, {
+            path: '/2017/01/02/hello-again/',
+            users: 7,
+            pgeviews: 9
+        }
+
+    ]
+};
+
+console.log(_.pick(day, ['date', 'users']));

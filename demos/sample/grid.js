96ce862a2127791dba3da3e5f337ad48c92fb55c
dustinpfister <dustin.pfister@gmail.com>
2018-07-13 10:37:15 -0400

    chunkToCols

--- demos/sample/grid.js
+++ demos/sample/grid.js
@@ -10,9 +10,20 @@ let grid = {
         {gold: 0,wood:0},{gold: 10,wood:0},{gold: 0,wood:6}],
 
     // chunk the linear array into an array of arrays
-    chunkIt: function () {
+    // where each array is a row
+    chunkToRows: function () {
 
+        // so _.chunk makes quick work of this
         return _.chunk(this.cells, this.w)
+
+    },
+
+    // chunk the linear array into an array of arrays
+    // where each array is a col (rotated right)
+    chunkToCols : function(){
+
+        // _.zip is useful for doing this
+        return _.zip.apply(null, this.chunkToRows());
 
     },
 
@@ -32,12 +43,32 @@ let grid = {
 
         });
 
+    },
+
+    // get a Random row, col or cell
+    getRnd: function(what){
+
+        what = what || 'cell';
+
+		/*
+		if(what === 'row'){
+			
+			return this.getRow
+			
+		}
+		*/
+		
+        // for a random cell just return a sample from the whole set
+        return _.sample(this.cells);
+
     }
 
 };
 
+console.log(grid.chunkToCols());
+
 // get row 1
-console.log(grid.getRow(1));
+//console.log(grid.getRow(1));
 
 // get col 1
-console.log(grid.getCol(1));
+//console.log(grid.getCol(1));

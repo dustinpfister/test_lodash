279f5deb873b59be3de057025c7751672af0378b
dustinpfister <dustin.pfister@gmail.com>
2018-07-13 20:13:57 -0400

    matrix demo

---
+++ demos/chunk/marrix.js
@@ -0,0 +1,7 @@
+var _ = require('lodash'),
+ 
+// basic example
+arr = ['one', 'two', 'three', 'four', 'five', 'six'];
+ 
+console.log(_.chunk(arr, 2));
+// [ [ 'one', 'two' ], [ 'three', 'four' ], [ 'five', 'six' ] ]

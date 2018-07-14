d72f2bdc2d887765cd923cbe53e30345e08090d9
dustinpfister <dustin.pfister@gmail.com>
2018-07-13 20:21:24 -0400

    chunk and zip method

--- demos/chunk/basic.js
+++ demos/chunk/basic.js
@@ -1,42 +1,8 @@
-let _ = require('lodash');
+var _ = require('lodash'),
  
-// matrix data example
-data = {
+// basic example
+arr = ['one', 'two', 'three', 'four', 'five', 'six'];
  
-    w : 10,
-    colors : ['grey', 'blue','red'],
-    px : [
-        1,1,1,1,2,2,2,2,2,2,
-        1,0,0,1,0,0,0,0,0,0,
-        1,1,1,1,2,2,2,2,2,2,
-        0,0,0,0,0,0,0,0,0,0,
-        2,2,2,2,2,2,2,2,2,2],
- 
-    // my toMatrix method using _.chunk
-    toMatrix : function(){
- 
-        return _.chunk(this.px,this.w);
- 
-    }
- 
-};
- 
-// draw it out like this
-
-var html = '';
-data.toMatrix().forEach(function (line) {
- 
-    line.forEach(function (px) {
- 
-        html += '<span style=\"color: '+data.colors[px]+';\">X<\/span>';
- 
-    });
-    html += '<br>';
- 
-});
-
-console.log(html);
-/*
-<span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><br><span style="color: blue;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: blue;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><br><span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><br><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><br><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><br>
-*/
+console.log(_.chunk(arr, 2));
+// [ [ 'one', 'two' ], [ 'three', 'four' ], [ 'five', 'six' ] ]
 

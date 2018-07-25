d97a8c2214f3d1ef198791394099510be8f5d9aa
dustinpfister <dustin.pfister@gmail.com>
2018-07-25 10:38:43 -0400

    distance method

--- demos/mixin/newobj.js
+++ demos/mixin/newobj.js
@@ -1,16 +1,24 @@
 let _ = require('lodash');
 
+// just some properties
 let props = {
     x: 100,
     y: 100
 },
 
+// some methods that will work with such properties
 methods = {
     atan: function (string) {
         return Math.atan2(this.y, this.x) / Math.PI * 180;
+    },
+    dist: function (x, y) {
+
+        return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
+
     }
 }
 
 let obj = _.mixin(props, methods);
 
 console.log(obj.atan()); // 45
+console.log(obj.dist(0,0)); // 141.42...

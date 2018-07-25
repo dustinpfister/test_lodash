6e9b744182c1344babc4fcb9b1563f9a316d00ac
dustinpfister <dustin.pfister@gmail.com>
2018-07-25 10:49:49 -0400

    setByAD methods

--- demos/mixin/newobj.js
+++ demos/mixin/newobj.js
@@ -12,13 +12,24 @@ methods = {
         return Math.atan2(this.y, this.x) / Math.PI * 180;
     },
     dist: function (x, y) {
-
         return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
-
+    },
+    setByAD: function (a, d, cx, cy) {
+        cx = cx === undefined ? 0 : cx;
+        cy = cy === undefined ? 0 : cy;
+        a = Math.PI / 180 * a;
+        this.x = Math.cos(a) * d + cx;
+        this.y = Math.sin(a) * d + cy;
+        return this;
     }
-}
+};
 
 let obj = _.mixin(props, methods);
 
+// atan, and distance relative to 0,0
 console.log(obj.atan()); // 45
-console.log(obj.dist(0,0)); // 141.42...
+console.log(obj.dist(0, 0)); // 141.42...
+
+// set by angle and distance
+obj.setByAD(45, 100);
+console.log(_.round(obj.dist(0, 0))); // 100

39354ff742e41056e30865e1a2296a494c3a315d
dustinpfister <dustin.pfister@gmail.com>
2018-07-25 08:46:29 -0400

    reduce object method

--- demos/reduce/object_vjs.js
+++ demos/reduce/object_vjs.js
@@ -1,6 +1,6 @@
 let _ = require('lodash');
 
-let sum = Array.prototype.reduce.call({0:1,1:1,2:1,3:1,length:4}, function (sum, cur) {
+let sum = Array.prototype.reduce.call({0: 1,1: 1,2: 1,3: 1,length: 4},(sum, cur)=>{
 
     return sum + cur;
 
@@ -8,3 +8,24 @@ let sum = Array.prototype.reduce.call({0:1,1:1,2:1,3:1,length:4}, function (sum,
 
 console.log(sum); // 4
 
+
+let sumObj = function (obj) {
+
+    let nObj = Object.assign({}, obj, {
+            length: Object.keys(obj).length
+        });
+
+    return Array.prototype.reduce.call(nObj,function (sum, cur) {
+
+        return sum + cur;
+
+    });
+
+};
+
+console.log(sumObj({
+        0: 1,
+        1: 1,
+        2: 1,
+        3: 1
+    }));

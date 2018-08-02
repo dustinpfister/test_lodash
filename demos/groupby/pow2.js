06541a1e9828720ef5ac471f6e8ffa16c33084e7
dustinpfister <dustin.pfister@gmail.com>
2018-08-02 19:19:50 -0400

    better basic demo

--- demos/groupby/pow2.js
+++ demos/groupby/pow2.js
@@ -4,7 +4,7 @@ let nums = [2,20,4,3,7,8,32,42,256];
 
 let grouped = _.groupBy(nums, function (n) {
 
-    return n < 10 ? 'fail' : 'pass'
+    return String(Math.log(n) / Math.log(2)).indexOf('.') === -1 ? 'pow2': 'notPow2';
 
     });
 

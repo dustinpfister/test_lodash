98f8c678675f8f7aaaac6ae6f6379dd13e8cca8d
dustinpfister <dustin.pfister@gmail.com>
2018-08-02 17:44:20 -0400

    comments

--- demos/concat/multi1.js
+++ demos/concat/multi1.js
@@ -8,6 +8,10 @@ let full = _.concat(arr1,arr2);
 
 console.log(full); // [ 1, 2, [ 3, 4, [ 5, 6 ] ], 7, 8, 9 ]
 
+// arrays are a kind of object,
+// and they are copied by reference
+// so a change like this
 arr1[2][0] = -1;
 
+// will make a change to what is made with concat as well
 console.log(full); // [ 1, 2, [ -1, 4, [ 5, 6 ] ], 7, 8, 9 ]

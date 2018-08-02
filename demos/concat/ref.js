860556027b340d535e323fe134d1a9b93a0b7545
dustinpfister <dustin.pfister@gmail.com>
2018-08-02 18:47:09 -0400

    ref demo

--- demos/concat/ref.js
+++ demos/concat/ref.js
@@ -1,11 +1,16 @@
-let _ = require('lodash'),
-objs = [{x:1,y:5}, {x:7,y:10}];
+let _ = require('lodash');
 
-// from an empty array
-let points = [].concat(objs, {x:0,y:0});
+// some objects
+let objs = [{x:1,y:5}, {x:7,y:10}];
 
+// concatenating with another object
+let points = _.concat(objs, {x:0,y:0});
+
+// works as exspected
 console.log(points); // [ { x: 1, y: 5 }, { x: 7, y: 10 }, { x: 0, y: 1 } ]
 
+// but what if the primitives in the referenced
+// objects change?
 _.each(objs, function(pt){
 
     pt.x = 0;
@@ -13,4 +18,5 @@ _.each(objs, function(pt){
 
 });
 
+// this is what
 console.log(points); // [ { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 } ]

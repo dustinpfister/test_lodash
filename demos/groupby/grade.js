a4b6b31fefb0af9d5a764109640702b14b9d8564
dustinpfister <dustin.pfister@gmail.com>
2018-08-03 11:54:54 -0400

    grade method

--- demos/groupby/grade.js
+++ demos/groupby/grade.js
@@ -1,45 +1,35 @@
 let _ = require('lodash');
 
-let scores = [{
-        name: 'Math',
-        score: 83
-    }, {
-        name: 'Programing',
-        score: 100
-    }, {
-        name: 'Art',
-        score: 98
-    }, {
-        name: 'PE',
-        score: 93
-    }, {
-        name: 'English',
-        score: 42
-    }, {
-        name: 'Bio',
-        score: 58
-    }
+let clases = [
+    {name: 'Math',score: 83},
+    {name: 'Programing',score: 100},
+    {name: 'Art',score: 98}, {name: 'PE',score: 93},
+    {name: 'English',score: 42},
+    {name: 'Bio',score: 60}
 ];
 
-let grade = _.groupBy(scores, function (sub) {
+// grade method that uses _.groupBy
+let grade = function (clases) {
 
-        let letters = {
-            'A+': 98,
-            'A.': 93,
-            'A-': 90,
-            'B+': 86,
-            'B.': 83,
-            'B-': 80,
-            'C+': 76,
-            'C.': 73,
-            'C-': 70,
-            'D+': 66,
-            'D.': 63,
-            'D-': 60
-        },
+    let letters = {
+        'A+': 98,
+        'A.': 93,
+        'A-': 90,
+        'B+': 86,
+        'B.': 83,
+        'B-': 80,
+        'C+': 76,
+        'C.': 73,
+        'C-': 70,
+        'D+': 66,
+        'D.': 63,
+        'D-': 60
+    };
+
+    return _.groupBy(clases, function (sub) {
 
         // default to an F
-        key = 'F.';
+        let key = 'F.';
 
         _.forEach(letters, function (g, letter) {
 
@@ -56,4 +46,6 @@ let grade = _.groupBy(scores, function (sub) {
 
     });
 
-console.log(grade)
+};
+
+console.log(grade(clases));

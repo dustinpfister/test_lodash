080cd2f7dfdc6b7885086beadd351def695cd5f2
dustinpfister <dustin.pfister@gmail.com>
2018-08-03 18:07:15 -0400

    pad demos

---
+++ demos/pad/basic.js
@@ -0,0 +1,8 @@
+let _ = require('lodash');
+
+
+let an = 1503345;
+
+console.log(_.pad(an,10,'0'));      // 0150334500
+console.log(_.padEnd(an,10,'0'));   // 1503345000
+console.log(_.padStart(an,10,'0')); // 0001503345

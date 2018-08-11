25e85c60faac709c87055b5c7beb578b30274e1f
dustinpfister <dustin.pfister@gmail.com>
2018-08-11 14:45:38 -0400

    clean up

--- demos/fill/tostring_vjs.js
+++ demos/fill/tostring_vjs.js
@@ -1,12 +1,9 @@
-let _ = require('lodash');
+var b = new Array(8).fill(0).join('');
 
-var b = new Array(8).fill(0).join(''); // [ '0', '0', '0', '0', '0', '0', '0', '0' ]
-
-// to string with \_.join
-
+// to string with Array.join
 console.log(b); // '00000000'
 
-// back again with _.split
+// back again with Array.split
 console.log(b.split('')); // [ '0', '0', '0', '0', '0', '0', '0', '0' ]
 
 

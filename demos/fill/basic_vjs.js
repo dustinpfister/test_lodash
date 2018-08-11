edd57e2f9c0bfea6b7947a7f15a3981b6a1d9975
dustinpfister <dustin.pfister@gmail.com>
2018-08-11 14:03:37 -0400

    vanilla js solution has a pollyfill

--- demos/fill/basic_vjs.js
+++ demos/fill/basic_vjs.js
@@ -1,2 +1,23 @@
+
+Array.prototype.fill = Array.prototype.fill || function (val, start, end) {
+
+    var i;
+
+    start = start === undefined ? 0 : start;
+    end = end === undefined ? this.length : end;
+
+    i = start;
+    while (i < end) {
+
+        this[i] = val;
+        i += 1;
+    }
+
+    return this;
+
+};
+
 var b = new Array(8).fill(0);
-console.log(b); // [0,0,0,0,0,0,0,0]
+
+console.log(b);
+

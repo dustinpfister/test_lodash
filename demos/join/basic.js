222c4c4f21e789e3d1107492ed5588cc908e5fcb
dustinpfister <dustin.pfister@gmail.com>
2018-08-11 15:27:50 -0400

    basic vjs

--- demos/join/basic.js
+++ demos/join/basic.js
@@ -2,6 +2,6 @@ let _ = require('lodash');
 
 var str = _.join(['home','dustin','github','test_lodash'], '/');
 
-console.log(str);
+console.log(str); //'home/dustin/github/test_lodash'
 
 console.log(_.split(str,'/')); // [ 'home', 'dustin', 'github', 'test_lodash' ]
---
+++ demos/join/basic_vjs.js
@@ -0,0 +1,5 @@
+var str = ['home','dustin','github','test_lodash'].join('/');
+
+console.log(str); //'home/dustin/github/test_lodash'
+
+console.log(str.split('/')); // [ 'home', 'dustin', 'github', 'test_lodash' ]

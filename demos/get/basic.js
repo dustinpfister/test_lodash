49fb2a0b22d85041ef664dd44fcb9eb18427b629
dustinpfister <dustin.pfister@gmail.com>
2018-09-24 12:59:15 -0400

    default example

--- demos/get/basic.js
+++ demos/get/basic.js
@@ -17,6 +17,22 @@ let enemy = {
 
 };
 
+// can get plain old object properties like this
 console.log(_.get(enemy, 'health.healRate.active')); // false
 
+// if it is an array I can and element get like this
+// if I know the index
 console.log(_.get(enemy, 'targets[2]')); // 8
+
+// or like this
+console.log(_.get(enemy, 'targets.2')); // 8
+
+
+console.log(enemy.health.healRate.active); // false
+console.log(enemy['health']['healRate']['active']); // false
+
+// default
+console.log(_.get(enemy, 'health.healRate.overTime', {
+   amount: 0,
+   ticks: 0
+}));

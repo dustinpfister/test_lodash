de505d58d79b2312804fbb12a4eb9b8aae685f54
dustinpfister <dustin.pfister@gmail.com>
2018-09-27 17:36:27 -0400

    started demos for create

---
+++ demos/create/basic.js
@@ -0,0 +1,18 @@
+let _ = require('lodash');
+
+var methods = {
+
+    move: function (x, y) {
+
+        this.x += x;
+        this.y += y;
+
+    }
+
+};
+
+var obj = _.create(methods, {x: 5,y: 15});
+
+obj.move(5, 5);
+
+console.log(obj.x, obj.y); // 10 20

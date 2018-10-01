5963ab964092d501d3208edd213a7de184e97440
dustinpfister <dustin.pfister@gmail.com>
2018-09-30 20:30:11 -0400

    Object.keys solution

--- demos/forin/basic_vjs.js
+++ demos/forin/basic_vjs.js
@@ -10,16 +10,27 @@ A.prototype.c = 7;
 
 var a = new A();
 
+
 // a for in loop works
-for (var prop in a) {
-    console.log(a[prop]);
+for (let prop in a) {
+  console.log(prop + ' : ' +a[prop]);
 }
+
+
+let keys = Object.keys(a).concat(Object.keys(a.constructor.prototype));
+
+keys.forEach(function (key) {
+    var val = a[key];
+    console.log(key + ' : ' + val);
+});
+
 
 /*
 // Only loops over own properties
 
 //console.log(Object.keys(a));
 
+//console.log(Object.values(a))
 
 Object.getOwnPropertyNames(a).forEach(function(key){
 

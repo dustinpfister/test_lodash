2dd7959a90868a205c35e24bcb129f8c35415abe
dustinpfister <dustin.pfister@gmail.com>
2018-10-02 21:04:04 -0400

    steping

--- demos/range/basic.js
+++ demos/range/basic.js
@@ -8,3 +8,8 @@ console.log(nums); // [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 nums = _.range(5,10);
 
 console.log(nums); // [5,6,7,8,9]
+
+
+nums = _.range(100,200,20);
+
+console.log(nums); //  [ 100, 120, 140, 160, 180 ]

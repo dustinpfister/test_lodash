18b386ebdd68faa3d781b550e32d069a78f4fb17
dustinpfister <dustin.pfister@gmail.com>
2018-10-03 18:10:39 -0400

    basic demo

--- demos/uniqueid/basic.js
+++ demos/uniqueid/basic.js
@@ -1,8 +1,14 @@
 let _ = require('lodash');
 
-
 let id = _.uniqueId('id_');
 
 console.log(id); // 'id_1'
 
+let i = 10, ids = [];
+while (i--) {
+    ids.push(_.uniqueId('id_'));
+}
+
+console.log(ids[0]); // id_2
+console.log(ids[9]); // id_11
 

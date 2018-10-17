81d0184df5204db5f0b5e2cd6caff00e8b2e2e7b
dustinpfister <dustin.pfister@gmail.com>
2018-10-17 13:18:04 -0400

    array.from example

--- demos/reverse/custom.js
+++ demos/reverse/custom.js
@@ -14,5 +14,9 @@ console.log(reverse([1,2,3,4])); // [4,3,2,1]
 // object.assign
 let arr = [1,2,3,4],
 arr2 = Array.from(Array.prototype.reverse.call(Object.assign({length:Object.keys(arr).length},arr)));
-console.log(arr);
-console.log(arr2);
+console.log(arr); // [ 1, 2, 3, 4 ]
+console.log(arr2); // [ 4, 3, 2, 1 ]
+
+// array.from
+console.log( Array.from(arr).reverse()); // [ 4, 3, 2, 1 ]
+console.log(arr); // [ 1, 2, 3, 4 ]

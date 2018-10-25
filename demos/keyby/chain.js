95b444c8742a5b84f6a31deb170e50afa0dbec05
dustinpfister <dustin.pfister@gmail.com>
2018-10-24 20:16:21 -0400

    names

--- demos/keyby/chain.js
+++ demos/keyby/chain.js
@@ -3,28 +3,32 @@ let _ = require('lodash');
 let resp = [{
         'id': 'soups',
         'recipes': [{
-                'id': 's1'
+                'id': 's1',
+                'name': 'chicken soup'
             }, {
-                'id': 's2'
+                'id': 's2',
+                'name': 'french onion'
             }
         ]
     }, {
         'id': 'salads',
         'recipes': [{
-                'id': 's1'
+                'id': 's1',
+                'name': 'caesar salad'
             }, {
-                'id': 's2'
+                'id': 's2',
+                'name': 'chef salad'
             }
         ]
     }
 ];
 
 let keyed = _.chain(resp)
-.keyBy('id')
-.mapValues(function (item) {
-    item.recipes = _.keyBy(item.recipes, 'id');
-    return item;
-})
-.value();
+    .keyBy('id')
+    .mapValues(function (item) {
+        item.recipes = _.keyBy(item.recipes, 'id');
+        return item;
+    })
+    .value();
 
-console.log(keyed)
+console.log(keyed.soups.recipes.s1.name); // 'chicken soup'

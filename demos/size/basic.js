63603a7af27d29bcc22c58a32c8506f3f272bc6f
dustinpfister <dustin.pfister@gmail.com>
2018-11-04 16:28:03 -0500

    comments

--- demos/size/basic.js
+++ demos/size/basic.js
@@ -1,14 +1,14 @@
 let _ = require('lodash');
 
+// array element length
 var a = [1, 2, 3, 4];
-
 console.log(_.size(a)); // 4
 
+// Object key length
 var b = {
     a: 1,
     b: 2,
     c: 3,
     d: 4
 };
-
 console.log(_.size(b)); // 4
--- demos/size/basic_vjs.js
+++ demos/size/basic_vjs.js
@@ -1,13 +1,13 @@
 
+// array element length
 var a = [1, 2, 3, 4];
-
 console.log(a.length); // 4
 
+// Object key length
 var b = {
     a: 1,
     b: 2,
     c: 3,
     d: 4
 };
-
 console.log(Object.keys(b).length); // 4

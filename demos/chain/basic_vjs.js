9518e3be84699684c216937aa97cf12604cbdd12
dustinpfister <dustin.pfister@gmail.com>
2018-11-11 11:16:06 -0500

    native chaining

--- demos/chain/basic_vjs.js
+++ demos/chain/basic_vjs.js
@@ -1,12 +1,14 @@
 let _ = require('lodash');
 
+let arr = [1, 2, 3, 4]
+.map((n) => {
+    return Math.pow(2, n);
+})
+.filter((n) => {
+    return n % 8 === 0;
+})
+.reduce((acc, n) => {
+    return acc + n
+});
 
-let arr = [1,2,3,4]
-    .map((n) => {
-        return Math.pow(2, n);
-    })
-    .filter((n) => {
-        return n % 8 === 0;
-    });
-
-console.log(arr); 
+console.log(arr);

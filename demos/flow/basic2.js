326e193f2b2d4285f25fbfeb3b8ddb54489a24fe
dustinpfister <dustin.pfister@gmail.com>
2018-11-23 11:20:57 -0500

    basic2

---
+++ demos/flow/basic2.js
@@ -0,0 +1,17 @@
+let _ = require('lodash');
+
+/*
+let forA = function (a1, a2) {
+return Math.pow(a1 - a2, 2);
+};
+ */
+
+let dist = _.flow([
+    function (x1, y1, x2, y2) {
+        return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
+    },
+    Math.sqrt,
+    Math.round
+]);
+
+console.log(dist(10, 15, 90, 22)); // 80

3505b18818d73a975d6c6acb11bf1cf1936700c1
dustinpfister <dustin.pfister@gmail.com>
2018-12-03 17:12:13 -0500

    chain example looking good

--- demos/split/chain.js
+++ demos/split/chain.js
@@ -6,6 +6,11 @@ let arr = _.chain(css)
 .split(';')
 .join(':')
 .split(':')
+.chunk(2)
+.initial()
 .value();
 
 console.log(arr);
+// [ [ 'color', 'red' ],
+//  [ 'font-size', '12pt' ],
+//  [ 'font-family', 'arial' ] ]

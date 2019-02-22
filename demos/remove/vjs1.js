74f73a2b5d1fe3e0b4f278837097b1fefc6ec48d
dustinpfister <dustin.pfister@gmail.com>
2019-02-21 22:21:27 -0500

    looping backwards

--- demos/remove/vjs1.js
+++ demos/remove/vjs1.js
@@ -2,13 +2,22 @@
 var arr = [-1, -2, 3, -4, 5],
 i = 0, len = arr.length;
 while (i < len) {
-
     var el = arr[i];
     if (el < 0) {
         arr.splice(i, 1);
     }
-
     i += 1;
 }
 
 console.log(arr); // [ -2, 3, 5 ]
+
+// looping backwards also works
+var arr = [-1, -2, 3, -4, 5],
+i = arr.length;
+while (i--) {
+    var el = arr[i];
+    if (el < 0) {
+        arr.splice(i, 1);
+    }
+}
+console.log(arr); // [3,5]

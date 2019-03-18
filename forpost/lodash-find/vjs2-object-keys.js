dc9a72178c08577a20e5dc813578363181710846
dustinpfister <dustin.pfister@gmail.com>
2019-03-18 18:25:26 -0400

    vjs object keys obj

--- forpost/lodash-find/vjs2-object-keys.js
+++ forpost/lodash-find/vjs2-object-keys.js
@@ -12,8 +12,22 @@ var find = function (col, forEach) {
 
 var nums = [7, 8, 13, -5, 32, 2.5];
 var pow2 = find(nums, function (val, key) {
-    var p = Math.log(val) / Math.log(2);
-    return p > 0 && String(p).indexOf('.') === -1;
-});
+        var p = Math.log(val) / Math.log(2);
+        return p > 0 && String(p).indexOf('.') === -1;
+    });
 
 console.log(pow2); // [32,8]
+
+var obj = {
+
+    foo: 'bar',
+    n: 42,
+    mess: 'hello world',
+    bar: true,
+    baz: null
+};
+
+var strings = find(obj, function (val) {
+        return typeof val === 'string';
+    });
+console.log(strings); // ['hello world', 'bar']

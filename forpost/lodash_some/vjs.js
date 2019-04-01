580969b785929efb1ed0bfc36cab0bc9a8fe5df1
dustinpfister <dustin.pfister@gmail.com>
2019-04-01 12:37:30 -0400

    vjs some examples

--- forpost/lodash_some/vjs.js
+++ forpost/lodash_some/vjs.js
@@ -11,8 +11,25 @@ let some = function (col, tester) {
     return false;
 };
 
+// works with arrays
 let arr = [null, 'foo', 'baz', 42, {}, false, true];
+console.log(some(arr, function (el) {
+        return typeof el === 'number';
+    })); // true
+console.log(some(arr, function (el) {
+        return typeof el > 50
+    })); // false
 
-console.log( some(arr, function (el) {
-    return typeof el === 'number';
-}) );
+// works with objects in general
+let obj = {
+    x: 42,
+    y: 17,
+    z: -12
+};
+
+console.log(some(obj, function (n) {
+        return n > 50 || n < -50
+    })); // false
+console.log(some(obj, function (n) {
+        return n <0
+    })); // true

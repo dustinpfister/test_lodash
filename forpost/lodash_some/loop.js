614512a6c1555107df85680e8a5564e33de757ea
dustinpfister <dustin.pfister@gmail.com>
2019-04-05 12:58:58 -0400

    loop example

---
+++ forpost/lodash_some/loop.js
@@ -0,0 +1,12 @@
+let arr = ['foo', 42, null, 'baz'];
+
+let i = arr.length,
+some = false;
+while (i--) {
+    if (arr[i] === null) {
+        some = true;
+        break;
+    }
+}
+
+console.log(some);

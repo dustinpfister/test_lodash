9cdad4bed0eeb91fd1749c4b62b96d7f1d09a31f
dustinpfister <dustin.pfister@gmail.com>
2019-05-14 16:34:58 -0400

    pollyfill

--- forpost/lodash_keys/forin.js
+++ forpost/lodash_keys/forin.js
@@ -5,6 +5,7 @@ let obj = {
     n: 42
 };
 
+// stand alone method
 var keys = function (obj) {
     var keyNames = [],
     keyName;
@@ -15,3 +16,15 @@ var keys = function (obj) {
 };
 
 console.log( keys(obj) ); // ['foo', 'n']
+
+// polyfill
+Object.keys = Object.keys || function (obj) {
+    var keyNames = [],
+    keyName;
+    for (keyName in obj) {
+        keyNames.push(keyName);
+    }
+    return keyNames;
+};
+
+console.log( Object.keys(obj) );

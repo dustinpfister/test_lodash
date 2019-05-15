4cb8f4b64e3ea50c63b5d12833e49852014a3424
dustinpfister <dustin.pfister@gmail.com>
2019-05-15 15:26:27 -0400

    remaking has is a lttle involved

--- forpost/lodash_has/has_vjs.js
+++ forpost/lodash_has/has_vjs.js
@@ -2,17 +2,44 @@
 let has = (obj, path) => {
     try {
         let keys = path.split('.');
-        console.log(keys);
-		return true;
+        // console.log(keys);
+
+        let t = obj;
+        keys.forEach((key, i) => {
+
+            t = t[key];
+
+            console.log(key)
+            console.log(t);
+
+            /*
+            if(i === keys.length-1){
+
+            console.log(key);
+            console.log(t);
+
+            }*/
+        });
+
+        if (obj) {
+            //if(key in obj){
+            return true;
+            //}
+        }
+
     } catch (e) {
+        console.log(e.message)
         return false;
     }
+    return false;
 };
 
 let ctx = {
     draw: {
-        arc: true
+        arc: undefined
     }
 };
 
-console.log( has(ctx,'draw.arc') );
+console.log(has(ctx, 'draw.arc')); // true
+//console.log(has(ctx, 'draw.foo')); // false
+//console.log(has(ctx, 'draw.foo.bar')); // false

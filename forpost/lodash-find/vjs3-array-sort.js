1e5246739296734c52a52ab80f2700fc42267a29
dustinpfister <dustin.pfister@gmail.com>
2019-06-25 17:30:47 -0400

    great example

--- forpost/lodash-find/vjs3-array-sort.js
+++ forpost/lodash-find/vjs3-array-sort.js
@@ -1,14 +1,11 @@
-let arr = [{
-        text: 'In this post I will be writing about something else compleatly'
-    }, {
-        text: 'This is on lodash find and other topics related to lodash find'
-    }, {
-        text: 'Sorry not what you want to find here'
-    }, {
-        text: 'This is on lodash but not what you are looking for so you must find it elseware'
-    },
+let arr = [
+    {text: 'In this post I will be writing about something else completely'}, 
+    {text: 'This is on lodash find and other topics related to lodash find'}, 
+    {text: 'Sorry not what you want to find here'}, 
+    {text: 'This is on lodash but not what you are looking for so you must find it elsewhere'}
 ];
 
+// get a score based on count of term words
 let getWordScore = (str, term) => {
     let words = str.split(' ');
     return words.map((w) => {
@@ -25,14 +22,25 @@ let getWordScore = (str, term) => {
     });
 };
 
-let getScore = (str,term) => {
+// get a score based on full term match
+let getFullTermScore = (str, term) => {
     let full = str.match(new RegExp(term, 'g'));
-    full = full === null ? 0 : full.length;
-    return full + getWordScore(str, term);
+    return full === null ? 0 : full.length;
 };
 
-let term = 'lodash find';
-arr = arr.sort((a, b) => {
+// get score for a string and term
+let getScore = (str, term) => {
+    return getFullTermScore(str, term) + getWordScore(str, term);
+};
+
+let find = (arr, term, count) => {
+
+    // work with a copy
+    arr = arr.slice(0, arr.length);
+
+    count = count || 1;
+
+    arr.sort((a, b) => {
         if (getScore(a.text, term) > getScore(b.text, term)) {
             return -1;
         }
@@ -42,4 +50,19 @@ arr = arr.sort((a, b) => {
         return 0;
     });
 
-console.log(arr);
+    if (count === 1) {
+        return arr[0];
+    } else {
+        return arr.slice(0, count);
+    }
+
+};
+
+// just one
+console.log( find(arr, 'lodash find') );
+// { text: 'This is on lodash find and other topics related to lodash find' }
+
+// top two results
+console.log( find(arr, 'lodash find',2) );
+// [ { text: 'This is on lodash find and other topics related to lodash find' },
+//   { text: 'This is on lodash but not what you are looking for so you must find it elseware' } ]

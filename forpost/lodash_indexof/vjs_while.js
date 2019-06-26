57b33f866cebf34391df23f38ba120bc621e35b4
dustinpfister <dustin.pfister@gmail.com>
2019-06-26 13:36:13 -0400

    while example works with arrays of objects

--- forpost/lodash_indexof/vjs_while.js
+++ forpost/lodash_indexof/vjs_while.js
@@ -23,12 +23,33 @@ let findIndexes = (opt) => {
     return indexes;
 };
 
+// check it
 let indexes = findIndexes({
         arr: ['zero', 'one', 'two', 'three', 'two'],
         term: 'two'
     });
-
 // first from the left
 console.log(indexes[0]); // 2
 // all of them
 console.log(indexes); // [2,4]
+
+// Now for an array of objects
+let postIndexes = findIndexes({
+        arr: [
+            {cat:'lodash', name:'lodash_indexof',wordCount:250},
+            {cat:'lodash', name:'lodash-find',wordCount:850},
+            {cat:'js', name:'js-fetch',wordCount:230},
+            {cat:'lodash', name:'lo-merge',wordCount:1250}
+        ],
+        term: 'lodash',
+        reverse: true,
+        lookFor: function(el, term){
+            if(el.cat === term && el.wordCount > 500){
+                return true;
+            }
+            return false;
+        }
+    });
+
+console.log(postIndexes[0]); // 3
+console.log(postIndexes); // [3,1]

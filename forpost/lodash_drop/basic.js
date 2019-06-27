5b97457d73a692db45ea91354483231808cb576e
dustinpfister <dustin.pfister@gmail.com>
2019-06-27 17:43:01 -0400

    lodash_drop folder

---
+++ forpost/lodash_drop/basic.js
@@ -0,0 +1,7 @@
+let lodash = require('lodash');
+let arr = [1,2,3,4,5,6],
+newArr = _.drop(arr,3);
+// the new Array is what remains
+console.log(newArr); // [4,5,6]
+// the original array is unchanged
+console.log(arr); // [1,2,3,4,5,6]

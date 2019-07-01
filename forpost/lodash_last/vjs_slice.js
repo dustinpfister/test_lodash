29b3b482045993c18c958165ef4873e4c3e2a3d8
dustinpfister <dustin.pfister@gmail.com>
2019-07-01 13:48:42 -0400

    splice and slice return arrays

--- forpost/lodash_last/vjs_slice.js
+++ forpost/lodash_last/vjs_slice.js
@@ -1,5 +1,5 @@
 let arr = [1, 2, 3, 4],
 last = arr.slice(arr.length-1,arr.length);
 // array.slice will work like lodash last
-console.log(last); // 4
+console.log(last[0]); // 4
 console.log(arr); // [1,2,3,4]
--- forpost/lodash_last/vjs_splice.js
+++ forpost/lodash_last/vjs_splice.js
@@ -2,5 +2,5 @@ let arr = [1, 2, 3, 4],
 last = arr.splice(arr.length-1,1);
 // array.splice will have the same effect as
 // arrar.pop
-console.log(last); // 4
+console.log(last[0]); // 4
 console.log(arr); // [1,2,3]

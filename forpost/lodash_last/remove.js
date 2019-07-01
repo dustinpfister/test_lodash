270820215681736c7511d1967752aaffee7b38a5
dustinpfister <dustin.pfister@gmail.com>
2019-07-01 13:20:03 -0400

    remove example

---
+++ forpost/lodash_last/remove.js
@@ -0,0 +1,12 @@
+_ = require('lodash');
+
+let arr = [1, 2, 3, 4],
+last = _.remove(arr, (el,i)=>{
+    return i === arr.length -1
+})
+// lodash remove could be used as well
+// for getting the last element. However it will
+// also mutate the array, and is a complex 
+// solution for a fairly simple task
+console.log(last); // 4
+console.log(arr); // [1,2,3,4]
--- forpost/lodash_last/vjs_splice.js
+++ forpost/lodash_last/vjs_splice.js
@@ -1,6 +1,6 @@
 let arr = [1, 2, 3, 4],
 last = arr.splice(arr.length-1,1);
-// array.alice will have the same effect as
+// array.splice will have the same effect as
 // arrar.pop
 console.log(last); // 4
 console.log(arr); // [1,2,3]

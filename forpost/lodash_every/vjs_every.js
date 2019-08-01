676cbd9dfe33f04441f88bc558f2be96cda39337
dustinpfister <dustin.pfister@gmail.com>
2019-08-01 13:22:10 -0400

    vjs_every example

---
+++ forpost/lodash_every/vjs_every.js
@@ -0,0 +1,9 @@
+
+let arr1 = [1, 2, 3, 4],
+arr2 = [1, 2, 'c', 4],
+tester = (el) => {
+    return typeof el === 'number';
+};
+// using the tester method with _.every
+console.log(arr1.every(tester)); // true
+console.log(arr2.every(tester)); // false

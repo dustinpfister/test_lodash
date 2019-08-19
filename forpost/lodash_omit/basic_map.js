3f474e7d69a4c951388442f711499591510a990a
dustinpfister <dustin.pfister@gmail.com>
2019-08-19 17:44:46 -0400

    commnets

--- forpost/lodash_omit/basic_map.js
+++ forpost/lodash_omit/basic_map.js
@@ -7,23 +7,23 @@ let obj = {
     z: 3
 };
 
+// start out with _.map and _.some
 let pairs = _.map(obj, (prop, key) => {
-
         return !_.some(['index', 'z'], (omitKey) => {
-
             return omitKey === key;
-
         }) ? [key, prop]: false;
-
     });
 
+// This gives me something to start with
 console.log(pairs);
 // [ false, [ 'x', 5 ], [ 'y', 12 ], false ]
 
+// then I use remove to have a clean set of pairs
 pairs = _.remove(pairs);
 console.log(pairs);
 // [ [ 'x', 5 ], [ 'y', 12 ] ]
 
+// now I can use that with _.fromPairs to get the new object
 let newObj = _.fromPairs(pairs);
 console.log(newObj);
 // { x: 5, y: 12 }

f1cf80ded089cc42292bf28ae2368929e8b332e3
dustinpfister <dustin.pfister@gmail.com>
2019-09-09 15:03:48 -0400

    vjs_once

---
+++ forpost/lodash_once/vjs_once.js
@@ -0,0 +1,18 @@
+let _ = require('lodash');
+
+let once = (func) => {
+    let calls = 1;
+    return function(){
+        if (calls > 0) {
+            func.apply(null, arguments);
+            calls--;
+        }
+    };
+};
+
+let trap = once((mess) => {
+        console.log(mess);
+    });
+
+trap('okay'); // 'okay' logged to the console
+trap('nope'); //

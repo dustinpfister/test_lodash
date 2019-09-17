b9b91867a22ba13b1f4bcc3bf591c635a31d6c65
dustinpfister <dustin.pfister@gmail.com>
2019-09-17 14:31:24 -0400

    basic example looking okay

--- forpost/lodash_words/basic.js
+++ forpost/lodash_words/basic.js
@@ -1,11 +1,6 @@
 let _ = require('lodash');
 
-let text = 'So lets have some fun with javaScript and words today',
+let text = 'So lets have some fun today',
 words = _.words(text);
-
-// almost words okay
 console.log(words);
-// ['So', 'lets', 'have', 'some', 'fun', 'with', 'java', 'Script', 'and', 'words', 'today']
-
-words = _.words(text.toLowerCase());
-console.log(words);
+// ['So', 'lets', 'have', 'some', 'fun', 'today']

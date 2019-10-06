3170a45900a17d4b07770712db185270a6457028
dustinpfister <dustin.pfister@gmail.com>
2019-10-06 18:26:14 -0400

    basic vjs map reduce and concat example for lodash flatmap

---
+++ forpost/lodash_flatmap/basic_vjs_map_reduce_concat.js
@@ -0,0 +1,13 @@
+let _ = require('lodash');
+
+let fromRed = (r) => {
+    let g = 128 + Math.floor(r / 255) * 64,
+    b = 255 - r;
+    return [r, g, b];
+}
+
+// array map, reduce, and concat is one option
+flat = [0, 255].map(fromRed).reduce((acc, val) => acc.concat(val));
+console.log(flat);
+// [ 0, 128, 255, 255, 192, 0 ]
+

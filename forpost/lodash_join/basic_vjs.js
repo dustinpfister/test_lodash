43382a100a3388b4179ed0b88eda05acadf7bacf
dustinpfister <dustin.pfister@gmail.com>
2019-11-01 10:04:47 -0400

    basic_vjs

---
+++ forpost/lodash_join/basic_vjs.js
@@ -0,0 +1,5 @@
+var str = ['home','dustin','github','test_lodash'].join('/');
+console.log(str); 
+console.log(str.split('/'));
+//'home/dustin/github/test_lodash'
+// [ 'home', 'dustin', 'github', 'test_lodash' ]

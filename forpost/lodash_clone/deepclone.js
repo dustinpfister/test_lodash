1ebb6b0d11e73d66d801d2d5517fe298027ca61b
dustinpfister <dustin.pfister@gmail.com>
2019-11-03 11:08:13 -0500

    deepclone

--- forpost/lodash_clone/deepclone.js
+++ forpost/lodash_clone/deepclone.js
@@ -10,10 +10,16 @@ let obj = {
     n: 42
 };
 
+// using clone to clone the object
 let a = _.clone(obj);
 
+// changing a value of the nested object
 a.point.x = 0;
 a.point.y = 0;
 
+// This effects the cloned object
+// as well as the original because the
+// nested object is still referenced rather than
+// copied
 console.log(obj.point.x, obj.point.y);
 console.log(a.point.x, a.point.y);

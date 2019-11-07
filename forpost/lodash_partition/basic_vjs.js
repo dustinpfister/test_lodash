4327c295180baa515ab105290f3daa00dfffc5a8
dustinpfister <dustin.pfister@gmail.com>
2019-11-07 11:14:23 -0500

    basic_vjs for lodash_partition

---
+++ forpost/lodash_partition/basic_vjs.js
@@ -0,0 +1,18 @@
+let _ = require('lodash');
+
+let arr = [8, null, 32, 'foo', NaN, 'bar', false, {}, 64, 128];
+let i = arr.length,
+parts = [[], []],
+condition = (el) => {
+    return typeof el === 'number' && !_.isNaN(el);
+};
+while (i--) {
+    let el = arr[i],
+    pi = condition(el) ? 0 : 1;
+    parts[pi].unshift(el);
+}
+console.log(parts[0]);
+// [ 8, 32, 64, 128 ]
+console.log(parts[1]);
+// [ null, 'foo', NaN, 'bar', false, {} ]
+

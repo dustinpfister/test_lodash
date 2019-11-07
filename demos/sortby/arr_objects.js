54516ea8b37fe3c2c3a9dd20070387589c64f362
dustinpfister <dustin.pfister@gmail.com>
2019-11-07 04:00:45 -0500

    arr_objects

--- demos/sortby/arr_objects.js
+++ demos/sortby/arr_objects.js
@@ -1,10 +1,16 @@
 let _ = require('lodash');
 
-let arr = [{x:42,y:12}, {x:23,y:32},{x:100,y:6}];
+let arr = [{x:20,y:20}, {x:23,y:32},{x:100,y:6}];
 
+// strings can be used to set a property to a number value
 let sortX = _.sortBy(arr, 'x'),
-sortY = _.sortBy(arr, 'y');
+sortY = _.sortBy(arr, 'y'),
+
+// or a function can be given to define some logic
+distance = _.sortBy(arr, (pt)=>{
+    return Math.sqrt( Math.pow(pt.x - 70, 2) + Math.pow(pt.y -70, 2) );
+});
 
 console.log(sortX.pop().x); // 100
 console.log(sortY.pop().y); // 32
-
+console.log(distance.pop().x); // 20

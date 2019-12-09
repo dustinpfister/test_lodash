10c5a9dd4244a1c641a9692fd01730c202ca144c
dustinpfister <dustin.pfister@gmail.com>
2019-12-09 09:04:49 -0500

    basic_vjs

--- forpost/lodash_intersection/basic_vjs.js
+++ forpost/lodash_intersection/basic_vjs.js
@@ -1,11 +1,29 @@
 let arr1 = [1, 4, 5, NaN],
-arr2 = [1, 2, 1, NaN],
-arr3 = [1, 4, 5];
+arr2 = [1, 2, 1, NaN];
 
-let result = arr1.filter((x) => {
-        return arr2.some((y) => {
+let intersectTwo = (a, b) => {
+    return a.filter((x) => {
+        return b.some((y) => {
             return Object.is(x, y);
         });
     });
+};
 
-console.log(result);
+let intersection = function () {
+
+    let r = [];
+
+    let i = 1;
+    while (i < arguments.length) {
+        r.push(intersectTwo(arguments[0], arguments[i]));
+        i += 1;
+    }
+
+    if (r.length != arguments.length - 1) {
+
+        return [];
+    }
+
+};
+
+console.log(intersection(arr1, arr2, arr3));

a4a7a79754219952564223a55596ff30eb40e27c
dustinpfister <dustin.pfister@gmail.com>
2020-02-02 11:06:14 -0500

    basic_vjs_while function

--- forpost/lodash_frompairs/basic_vjs_while.js
+++ forpost/lodash_frompairs/basic_vjs_while.js
@@ -1,12 +1,18 @@
 
+var fromPairs = function (arr) {
+    let i = 0,
+    len = arr.length,
+    pair,
+    obj = {};
+    while (i < len) {
+        pair = arr[i];
+        obj[pair[0]] = pair[1];
+        i += 1;
+    }
+    return obj;
+};
+
 let arr = [['x', 1], ['y', 2], ['z', 3]];
 
-let i = 0, len = arr.length, pair, obj = {};
-while (i < len) {
-    pair = arr[i];
-    obj[pair[0]] = pair[1];
-    i += 1;
-}
-
-console.log(obj);
+console.log(fromPairs(arr));
 // { x: 1, y: 2, z: 3 }

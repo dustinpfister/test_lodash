8fa728ab1036923be2f372c8b8baa41e777c5e5b
dustinpfister <dustin.pfister@gmail.com>
2020-02-03 05:27:12 -0500

    comments

--- forpost/lodash_words/set_pat.js
+++ forpost/lodash_words/set_pat.js
@@ -2,7 +2,14 @@ let _ = require('lodash');
 
 let text = 'This has the word javaScript in it';
 
-console.log( _.words(text, /\w+/g) );
+// just using lodash words by itself will give an unexpected result
+console.log( _.words(text) );
+// [ 'This', 'has', 'the', 'word', 'java', 'Script', 'in', 'it' ]
+
+// I could make all the words lowercase, but another option would be to use
+// a regular expression pattern
+console.log(  _.words(text.toLowerCase()) );
+// [ 'this', 'has', 'the', 'word', 'javascript', 'in', 'it' ]
 console.log(  _.words(text, /\w+/g) );
-console.log(  _.words(text, /\w+/g) );
+// [ 'this', 'has', 'the', 'word', 'javascript', 'in', 'it' ]
 

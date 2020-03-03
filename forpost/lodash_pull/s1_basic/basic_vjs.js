911a435edf004f7ff4bbf0e4d1918cad5676fe3c
dustinpfister <dustin.pfister@gmail.com>
2020-03-03 11:22:14 -0500

    basic_vjs

---
+++ forpost/lodash_pull/s1_basic/basic_vjs.js
@@ -0,0 +1,24 @@
+var pull = function (arr) {
+
+    var len = arguments.length,
+    val,
+    i = 1;
+
+    if (arguments.length <= 1) {
+        return arr || [];
+    }
+    while (i < len) {
+        val = arguments[i];
+        arr = arr.filter(function (el) {
+                return el != val;
+            });
+        i += 1;
+    }
+    return arr;
+};
+
+
+var arr = [-1, 5, 7, -1, -1, 8, 7];
+arr = pull(arr, -1);
+
+console.log(arr.join(':'));

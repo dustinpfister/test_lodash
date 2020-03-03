07ec611ca58aa1759d27a10b917616bdc34cb5d8
dustinpfister <dustin.pfister@gmail.com>
2020-03-03 11:54:32 -0500

    basic_remove updated

--- forpost/lodash_pull/s1_basic/basic_remove.js
+++ forpost/lodash_pull/s1_basic/basic_remove.js
@@ -2,7 +2,7 @@ let _ = require('lodash');
 
 let arr = [-1, 5, 7, -1, -1, 8, 7];
 
-// lodash \_.remove and \_.eq can be used also to do 
+// lodash \_.remove and \_.eq can be used also to do
 // the same thing more or less
 arr = _.remove(arr, function (el) {
         return !_.eq(el, -1);
@@ -10,3 +10,12 @@ arr = _.remove(arr, function (el) {
 
 console.log(_.join(arr, ':'));
 // 5:7:8:7
+
+// remove can also be used to define any custom
+// logic for removing elements
+
+var arr1 = _.remove([-1, 1, -5, -7, 2, 3], function (el) {
+        return !(el < 0);
+    });
+console.log(_.join(arr1, ':'));
+// '1:2:3'

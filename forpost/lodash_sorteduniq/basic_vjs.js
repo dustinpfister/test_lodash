c41e8ba2b9b70b4d0317a8e7390c3e9ed5fb708f
dustinpfister <dustin.pfister@gmail.com>
2020-06-01 16:07:36 -0400

    basic_vjs

--- forpost/lodash_sorteduniq/basic_vjs.js
+++ forpost/lodash_sorteduniq/basic_vjs.js
@@ -1,7 +1,9 @@
 
-let sortedUniq = arr => [...new Set(arr)].sort()
+let sortedUniq = arr => {
+    return [...new Set(arr)].sort()
+};
 
-let a = [1,1,2,2,3,3,4,4,4,4],
+let a = [1, 1, 2, 2, 3, 3, 4, 4, 4, 4],
 b = sortedUniq(a);
 
 console.log(b); // [1,2,3,4];

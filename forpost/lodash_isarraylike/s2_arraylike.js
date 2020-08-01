0bb86eb1cb18241568707043a000d5ab2ef29e1d
dustinpfister <dustin.pfister@gmail.com>
2020-08-01 17:52:09 -0400

    better example

--- forpost/lodash_isarraylike/s2_arraylike.js
+++ forpost/lodash_isarraylike/s2_arraylike.js
@@ -5,8 +5,16 @@ let obj = {
 },
 arr = [];
 
-console.log( _.isArray(obj) ); // false
-console.log( _.isArray(arr) ); // true
+console.log(_.isArray(obj)); // false
+console.log(_.isArray(arr)); // true
 
-console.log( _.isArrayLike(obj) ); // true
-console.log( _.isArrayLike(arr) ); // true
+console.log(_.isArrayLike(obj)); // true
+console.log(_.isArrayLike(arr)); // true
+
+//
+let isArrayLikeAndNotArray = (obj) => {
+    return _.isArrayLike(arr) && !_.isArray(obj);
+};
+
+console.log(  isArrayLikeAndNotArray(obj) ); // true
+console.log(  isArrayLikeAndNotArray(arr) ); // false

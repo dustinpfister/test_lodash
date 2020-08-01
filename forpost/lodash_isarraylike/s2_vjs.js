8d6a2b1bf15410c588d810adb9845454d93191e8
dustinpfister <dustin.pfister@gmail.com>
2020-08-01 17:11:41 -0400

    vjs example looking good

--- forpost/lodash_isarraylike/s2_vjs.js
+++ forpost/lodash_isarraylike/s2_vjs.js
@@ -1,4 +1,30 @@
-let _ = require('lodash');
+Number.isInteger = Number.isInteger || function(value) {
+  return typeof value === 'number' && 
+    isFinite(value) && 
+    Math.floor(value) === value;
+};
+
+let isArrayLike = function (a) {
+    // is it an object
+    if (typeof a != 'object' || a === null) {
+        return false;
+    }
+    // does it have a length property
+    if (a.length === undefined) {
+        return false;
+    }
+    // is length a number? is it an Integer?
+    // is it greater than 0 and less then or equal to MAX_SAFE_INTEGER
+    if (typeof a.length === 'number') {
+        if (Number.isInteger(a.length) && a.length > 0 && a.length <= Number.MAX_SAFE_INTEGER) {
+            // then it is array like
+            return true;
+        }
+    }
+    // if we get here it is not array like
+    return false;
+
+};
 
 let obj = {
     0: 1,
@@ -7,34 +33,7 @@ let obj = {
     length: 3
 };
 
-Number.isInteger = Number.isInteger || function (value) {
-    return typeof value === 'number' &&
-    isFinite(value) &&
-    Math.floor(value) === value;
-};
-
-let isArrayLike = function (obj) {
-
-    // is it an object
-    if (typeof a != 'object' || a === null) {
-        return false;
-    }
-
-    // does it have a length property
-    if (a.length === undefined) {
-        return false;
-    }
-
-    // is length a number? is it an Integer?
-    // is it greater than 0 and less then or equal to MAX_SAFE_INTEGER
-    if (typeof a.length === 'number') {
-        if (a.isInteger(a) && a > 0 && a <= Number.MAX_SAFE_INTEGER) {
-            // then it is array like
-            return true;
-        }
-    }
-
-    // if we get here it is not array like
-    return false;
-
-};
+console.log(isArrayLike('obj')); // false
+console.log(isArrayLike(null)); // false
+console.log(isArrayLike({})); // false
+console.log(isArrayLike(obj)); // true

163fbe413757da99f0bcac01fa9731b9a4b9f727
dustinpfister <dustin.pfister@gmail.com>
2020-09-03 08:47:05 -0400

    nested objects example

---
+++ forpost/lodash_union/s2_nested_objects.js
@@ -0,0 +1,29 @@
+let _ = require('lodash');
+
+let obj = {
+    i: 0
+};
+
+let a = [{
+        i: 1
+    }, obj],
+b = [[{
+            x: 2
+        },
+        obj
+    ]
+]
+
+let arr = _.union(a, b);
+
+// this will result in the nested array being one of the unique values
+// so the unique value will be the array, rather than the elements of it,
+// and the reference to the same object will be in there twice
+console.log(arr);
+//[ { i: 1 }, { i: 0 }, [ { x: 2 }, { i: 0 } ] ] 
+
+// so the _.flatten method can be used to flatten the nested array
+b = _.flatten(b);
+arr = _.union(a, b);
+console.log(arr);
+// [ { i: 1 }, { i: 0 }, { x: 2 } ]

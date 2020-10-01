c41a369e5d8284ce729f41b76cec819b7a709d05
dustinpfister <dustin.pfister@gmail.com>
2020-10-01 13:37:15 -0400

    vjs_exp

---
+++ forpost/lodash_constant/vjs_exp.js
@@ -0,0 +1,14 @@
+// is it really hard to make them with function
+// expressions too when it comes to ES5 javaScript?
+var returnTrue = function () {
+    return true;
+};
+console.log(returnTrue()); // true
+
+// I can use IIFEs when it comes to making them on the spot
+var n = (function () {
+    return 42
+}
+    ());
+
+console.log(n); // 42

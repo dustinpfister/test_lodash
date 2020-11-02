413807db9142b2edf9372dfd772cda8d5c8e46c8
dustinpfister <dustin.pfister@gmail.com>
2020-11-02 15:26:41 -0500

    s3_atan2_vjs example for lodash_rearg

---
+++ forpost/lodash_rearg/s3_atan2_vjs.js
@@ -0,0 +1,10 @@
+
+let atan2 = (x, y) => {
+    return Math.atan2(y, x);
+}
+
+let x = 45,
+y = 20;
+
+console.log(Math.atan2(y, x)); // 0.41822432957922906
+console.log(atan2(x, y)); // 0.41822432957922906

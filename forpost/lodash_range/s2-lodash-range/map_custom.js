7e1a790a914ee7dd3f9cd07f25b35411c96b156d
dustinpfister <dustin.pfister@gmail.com>
2021-10-03 11:47:39 -0400

    custom range method using map

--- forpost/lodash_range/s2-lodash-range/map_custom.js
+++ forpost/lodash_range/s2-lodash-range/map_custom.js
@@ -1,19 +1,31 @@
 let _ = require('lodash');
 
 const myRange = (start, size, step) => {
-    let startN = start,
-    arr = new Array(size),
-    nDelta = step === undefined ? 1 : step;
+    let arr = new Array(size);
+    step = step === undefined ? 1 : step;
     return _.map(arr, (el, i) => {
-        return startN + i * nDelta;
+        let d = step;
+        if (typeof step === 'function') {
+            d = step(i, start, size);
+        }
+        return start + i * d;
     });
 };
 
-console.log( myRange(0, 10) );
+console.log(myRange(0, 10));
 // [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 
-console.log( myRange(5, 15, 5) );
+console.log(myRange(5, 15, 5));
 // [ 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75 ]
 
-console.log( _.range(5, 15, 5) );
+console.log(_.range(5, 15, 5));
 // [5, 10]
+
+// I can still get a similar result to _.range if I just
+// adjust the arguments to size, rather than a stop value
+console.log(myRange(5, 2, 5));
+// [5, 10]
+
+// custom stepping
+console.log(myRange(0, 5, (i) => Math.pow(2, i)));
+// [ 0, 2, 8, 24, 64 ]

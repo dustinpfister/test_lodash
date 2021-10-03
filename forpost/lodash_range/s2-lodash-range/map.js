d2ae9f72ca625ec0e0d2c796c4730fd2accef757
dustinpfister <dustin.pfister@gmail.com>
2021-10-03 11:26:38 -0400

    cleaner map example

--- forpost/lodash_range/s2-lodash-range/map.js
+++ forpost/lodash_range/s2-lodash-range/map.js
@@ -1,8 +1,4 @@
 let _ = require('lodash');
 
-let nums = _.map(new Array(10), function (el, i) {
-        return i
-    });
-
+let nums = _.map(new Array(10), (el, i) => i);
 console.log(nums); // [0,1,2,3,4,5,6,7,8,9]
-

a423df1ec205ced0646e414f6b929ef273c2cde8
dustinpfister <dustin.pfister@gmail.com>
2021-10-03 11:38:31 -0400

    map_custom example looks good

--- forpost/lodash_range/s2-lodash-range/map_custom.js
+++ forpost/lodash_range/s2-lodash-range/map_custom.js
@@ -2,12 +2,18 @@ let _ = require('lodash');
 
 const myRange = (start, size, step) => {
     let startN = start,
-    arr = new Array(size);
-    nDelta = nDelta === undefined ? 1 : step;
+    arr = new Array(size),
+    nDelta = step === undefined ? 1 : step;
     return _.map(arr, (el, i) => {
         return startN + i * nDelta;
     });
 };
 
 console.log( myRange(0, 10) );
-,4,5,6,7,8,9]
+// [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+console.log( myRange(5, 15, 5) );
+// [ 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75 ]
+
+console.log( _.range(5, 15, 5) );
+// [5, 10]

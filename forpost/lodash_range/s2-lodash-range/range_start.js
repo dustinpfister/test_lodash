9e999c93c827cd1a6811028ed91663731512db32
dustinpfister <dustin.pfister@gmail.com>
2021-10-03 11:23:36 -0400

    delete old basic example

--- forpost/lodash_range/s2-lodash-range/range_start.js
+++ forpost/lodash_range/s2-lodash-range/range_start.js
@@ -2,3 +2,4 @@ let _ = require('lodash');
 
 let nums = _.range(5,10);
 console.log(nums); // [5,6,7,8,9]
+
--- forpost/lodash_range/s2-lodash-range/range_step.js
+++ forpost/lodash_range/s2-lodash-range/range_step.js
@@ -2,3 +2,4 @@ let _ = require('lodash');
 
 let nums = _.range(100,200,20);
 console.log(nums); //  [ 100, 120, 140, 160, 180 ]
+

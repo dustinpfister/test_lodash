68e1749ea6dd9e551fe4882d25a4b6e4587b76c5
dustinpfister <dustin.pfister@gmail.com>
2021-10-03 12:33:23 -0400

    removed white space

--- forpost/lodash_range/s3-vjs-range/range_while.js
+++ forpost/lodash_range/s3-vjs-range/range_while.js
@@ -1,35 +1,22 @@
 var range = function (start, end, step) {
-
     let arr = [],
     len = 0;
-
     step = step === undefined ? 1 : step;
-
     if (arguments.length === 1) {
-
         len = start;
         start = 0;
         end = start;
-
     } else {
-
         start = start === undefined ? 1 : start;
         end = end === undefined ? 1 : end;
         len = end - start;
-
     }
-
     var i = 0;
     while (i < len) {
-
         arr.push(start + i * step);
-
         i += 1;
-
     }
-
     return arr;
-
 };
 
 console.log( range(10) ); // [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]

15789ecfb676a6a92a25658e0332efbfad3c13a9
dustinpfister <dustin.pfister@gmail.com>
2021-10-05 09:19:55 -0400

    new basic example

--- forpost/lodash_countby/s1-basics/property.js
+++ forpost/lodash_countby/s1-basics/property.js
@@ -1,4 +1,18 @@
 let _ = require('lodash');
-console.log(_.countBy(['foo', 'man', 'chew', 'happy', 'bar'], (el) => {
-        return _.property('length')(el)
-    }));
+
+let words = ['foo', 'man', 'chew', 'happy', 'bar'];
+
+console.log( _.countBy(words, (el) => {
+        return el.length;
+    }) );
+
+console.log( _.countBy(words, (el) => {
+        return _.property('length')(el);
+    }) );
+
+console.log( _.countBy(words, _.property('length')) );
+
+console.log( _.countBy(words, 'length') );
+
+// all of these produce the same result
+// { '3': 3, '4': 1, '5': 1 }

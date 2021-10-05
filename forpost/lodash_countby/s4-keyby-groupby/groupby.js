7d57c145277242fcc662e54ea50f3e7fd115a8c0
dustinpfister <dustin.pfister@gmail.com>
2021-10-05 11:32:05 -0400

    groupby example looks good

---
+++ forpost/lodash_countby/s4-keyby-groupby/groupby.js
@@ -0,0 +1,18 @@
+let _ = require('lodash');
+
+let arr = [
+    { username: 'john', score: 37},
+    { username: 'jake', score: 67},
+    { username: 'bill', score: 20}
+];
+
+let func = (rec) => {
+    return rec.score >= 65 ? 'pass' : 'fail';
+};
+
+let keyed = _.groupBy(arr, func);
+console.log(keyed);
+/*
+{ fail: [{ username: 'john', score: 37},{ username: 'bill', score: 20 }],
+  pass: [{ username: 'jake', score: 67 }] }
+*/

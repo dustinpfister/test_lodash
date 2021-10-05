145684f12a9e0861302c76df6e6c4839ea8d8fb7
dustinpfister <dustin.pfister@gmail.com>
2021-10-05 11:28:30 -0400

    keyby example

---
+++ forpost/lodash_countby/s4-keyby-groupby/keyby.js
@@ -0,0 +1,24 @@
+let _ = require('lodash');
+
+let arr = [{
+        username: 'john',
+        score: 37
+    }, {
+        username: 'jake',
+        score: 67
+    }, {
+        username: 'bill',
+        score: 20
+    }
+];
+
+let func = (rec) => {
+    return rec.score >= 65 ? 'pass' : 'fail';
+};
+
+let keyed = _.keyBy(arr, func);
+console.log(keyed);
+/*
+{ fail: { username: 'bill', score: 20 },
+  pass: { username: 'jake', score: 67 } }
+*/

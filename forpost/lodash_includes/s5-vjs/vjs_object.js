7f696d6fc0db1fc28257761e40719b7cc072af06
dustinpfister <dustin.pfister@gmail.com>
2021-10-08 09:08:41 -0400

    clean up

--- forpost/lodash_includes/s5-vjs/vjs_object.js
+++ forpost/lodash_includes/s5-vjs/vjs_object.js
@@ -1,12 +1,5 @@
-let tester = (val) => {
-    return function (el) {
-        return el === val;
-    };
-};
-
 let obj = {
     a: 'foo',
     b: 'bar'
 };
-
-console.log( Object.values(obj).includes('foo') );
+console.log( Object.values(obj).includes('foo') ); // true

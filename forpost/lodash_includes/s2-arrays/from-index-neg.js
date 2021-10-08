ee6189f57e7aca59c17874a99813c01bec657b1b
dustinpfister <dustin.pfister@gmail.com>
2021-10-08 08:59:40 -0400

    from index examples

---
+++ forpost/lodash_includes/s2-arrays/from-index-neg.js
@@ -0,0 +1,4 @@
+let _ = require('lodash');
+
+
+console.log(_.includes(['a','b','c'],'b',-1)); // false
---
+++ forpost/lodash_includes/s2-arrays/from-index.js
@@ -0,0 +1,5 @@
+let _ = require('lodash');
+
+
+console.log(_.includes(['a','b','c'],'b',1)); // true
+console.log(_.includes(['a','b','c'],'b',2)); // false

e6835d990962f7c04dcce5f9573652023bbbc37a
dustinpfister <dustin.pfister@gmail.com>
2021-10-08 09:03:11 -0400

    clean up

--- forpost/lodash_includes/s5-vjs/vjs_some.js
+++ forpost/lodash_includes/s5-vjs/vjs_some.js
@@ -5,7 +5,6 @@ let tester = (val) => {
 };
 
 let arr = ['a', 'b', 'c'];
-
 console.log(arr.some( tester(42) )); // false
 console.log(arr.some( tester('d') )); // false
 console.log(arr.some( tester('c') )); // true

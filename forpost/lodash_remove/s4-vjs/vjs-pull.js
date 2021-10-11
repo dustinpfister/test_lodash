2a9b6b7dcbe2bb245e4ec70b7e11795f4c44803d
dustinpfister <dustin.pfister@gmail.com>
2021-10-11 11:36:35 -0400

    better object pull method using Object.is

--- forpost/lodash_remove/s4-vjs/vjs-pull.js
+++ forpost/lodash_remove/s4-vjs/vjs-pull.js
@@ -1,3 +1,4 @@
+// the remove method
 var remove = function (arr, func) {
     return arr.filter(function (el, i) {
         if (func.call(arr, el, i, arr)) {
@@ -7,6 +8,29 @@ var remove = function (arr, func) {
         return false;
     });
 };
+
+// Object.is polly fill
+if (!Object.is) {
+  Object.defineProperty(Object, "is", {
+    value: function (x, y) {
+      // SameValue algorithm
+      if (x === y) {
+        // return true if x and y are not 0, OR
+        // if x and y are both 0 of the same sign.
+        // This checks for cases 1 and 2 above.
+        return x !== 0 || 1 / x === 1 / y;
+      } else {
+        // return true if both x AND y evaluate to NaN.
+        // The only possibility for a variable to not be strictly equal to itself
+        // is when that variable evaluates to NaN (example: Number.NaN, 0/0, NaN).
+        // This checks for case 3.
+        return x !== x && y !== y;
+      }
+    }
+  });
+}
+
+// a pull method using Object.is in place of lodash _.eq
 var pull = function (arr, value) {
     return remove(arr, function (el) {
         return Object.is(el, value);
--- forpost/lodash_remove/todo.md
+++ forpost/lodash_remove/todo.md
@@ -1,5 +1,12 @@
 # lodash_remove todo
 
+## () - have a section on NOT monkey patching
+* this came up for 'javascript removing elements from array' in google on 10/11/21
+```
+    https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array
+    There is maybe linking to the page in the section, and outlining why not to do this
+```
+
 ## () - new section on object pools
 * start a new section on objects pools like that of what I am using in my canvas examples object pools example
 
@@ -9,4 +16,4 @@
 ## () - new s4-vjs examples
 * (done) have a a vjs-remove.js file that is a vjs remove method using native javaScript methods
 * have a slice concat example
-* have a vjs-pull.js by value method example using Object.is in place of lodash \_.eq
+* (done) have a vjs-pull.js by value method example using Object.is in place of lodash \_.eq

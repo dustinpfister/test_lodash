639c5440a115cae895a928b5b92986c87bdf05e5
dustinpfister <dustin.pfister@gmail.com>
2021-11-16 12:19:36 -0500

    comments

--- forpost/lodash_partition/s3-av-vjs/1-top-code.js
+++ forpost/lodash_partition/s3-av-vjs/1-top-code.js
@@ -1,11 +1,13 @@
-let arr = [8, null, 32, 'foo', NaN, 'bar', false, {}, 64, 128];
-let i = arr.length,
-parts = [[], []],
-condition = (el) => {
+let source = [8, null, 32, 'foo', NaN, 'bar', false, {}, 64, 128], // source array
+i = source.length, // index
+parts = [[], []],  // the parts array
+condition = (el) => { // a condition
     return typeof el === 'number' && !Number.isNaN(el);
 };
+// loop over elements of the source array, and call the condition function for each
+// the the return value of the condition is true, add to the first array, else the second
 while (i--) {
-    let el = arr[i],
+    let el = source[i],
     pi = condition(el) ? 0 : 1;
     parts[pi].unshift(el);
 }

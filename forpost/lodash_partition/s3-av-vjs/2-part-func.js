80ad745c97d0fe8f372dd25a770eddde43be289a
dustinpfister <dustin.pfister@gmail.com>
2021-11-16 12:27:10 -0500

    part func example

--- forpost/lodash_partition/s3-av-vjs/2-part-func.js
+++ forpost/lodash_partition/s3-av-vjs/2-part-func.js
@@ -1,16 +1,18 @@
-let arr = [8, null, 32, 'foo', NaN, 'bar', false, {}, 64, 128];
-let i = arr.length,
-parts = [[], []],
-condition = (el) => {
-    return typeof el === 'number' && !_.isNaN(el);
+let part = (source, condition) => {
+    i = source.length,
+    parts = [[], []],
+    condition = condition || function (el) {};
+    while (i--) {
+        let el = source[i],
+        pi = condition(el) ? 0 : 1;
+        parts[pi].unshift(el);
+    }
+    return parts;
 };
-while (i--) {
-    let el = arr[i],
-    pi = condition(el) ? 0 : 1;
-    parts[pi].unshift(el);
-}
-console.log(parts[0]);
-// [ 8, 32, 64, 128 ]
-console.log(parts[1]);
-// [ null, 'foo', NaN, 'bar', false, {} ]
 
+let source = [8, null, 32, 'foo', NaN, 'bar', false, {}, 64, 128]; // source array
+let b = part(source, (el) => {
+        return typeof el === 'number' && !Number.isNaN(el);
+    });
+console.log(b);
+//[ [ 8, 32, 64, 128 ], [ null, 'foo', NaN, 'bar', false, {} ] ]

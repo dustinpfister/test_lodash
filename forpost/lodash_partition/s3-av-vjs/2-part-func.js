f1d0d96e3d50a1afb929980177f4a96f193b8e17
dustinpfister <dustin.pfister@gmail.com>
2021-11-16 12:17:00 -0500

    clean up of top code example

--- forpost/lodash_partition/s3-av-vjs/2-part-func.js
+++ forpost/lodash_partition/s3-av-vjs/2-part-func.js
@@ -2,13 +2,15 @@ let arr = [8, null, 32, 'foo', NaN, 'bar', false, {}, 64, 128];
 let i = arr.length,
 parts = [[], []],
 condition = (el) => {
-    return typeof el === 'number' && !Number.isNaN(el);
+    return typeof el === 'number' && !_.isNaN(el);
 };
 while (i--) {
     let el = arr[i],
     pi = condition(el) ? 0 : 1;
     parts[pi].unshift(el);
 }
-console.log(parts);
-//[ [ 8, 32, 64, 128 ], [ null, 'foo', NaN, 'bar', false, {} ] ]
+console.log(parts[0]);
+// [ 8, 32, 64, 128 ]
+console.log(parts[1]);
+// [ null, 'foo', NaN, 'bar', false, {} ]
 
--- forpost/lodash_partition/todo.md
+++ forpost/lodash_partition/todo.md
@@ -5,6 +5,6 @@
 * example that uses the lodash partition method with an object
 
 ### () - expand av-vjs section
-* rename basic vjs to 1-top-code
+* (done) rename basic vjs to 1-top-code
 * 2-part-func example that is a function
 * 3-col example that will work with objects in general

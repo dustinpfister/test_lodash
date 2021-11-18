480e783499902f6622619f2e2a49f04701fabe4b
dustinpfister <dustin.pfister@gmail.com>
2021-11-18 14:25:13 -0500

    new basic example for lodash section

--- forpost/lodash_sum/s2-av-lodash/basic.js
+++ forpost/lodash_sum/s2-av-lodash/basic.js
@@ -1,8 +1,6 @@
 let _ = require('lodash');
 
 // array element length
-let a = [1, 2, 3, 4];
-
-let sum = _.sum(a);
-
+let a = [1, '2', 3, '4'];
+let sum = _(a).map((el)=>{return parseFloat(el)}).sum(a);
 console.log(sum); // 10

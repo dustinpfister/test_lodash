6043f9c607991820aed9609236f8a0947fd0d206
dustinpfister <dustin.pfister@gmail.com>
2021-11-24 10:59:02 -0500

    comments

--- forpost/lodash_throttle/s1-basics/2-app-loop.js
+++ forpost/lodash_throttle/s1-basics/2-app-loop.js
@@ -1,24 +1,25 @@
 let _ = require('lodash');
 
+// state machine object
 var sm = {
     currentState: 'init',
     states: {},
     lt: new Date()
 };
-
+// init state
 sm.states.init = {
     update: function (sm, secs) {
         console.log('starting app');
         sm.currentState = 'game';
     }
 };
-
+// game state
 sm.states.game = {
     update: function (sm, secs) {
         console.log('game update ' + secs);
     }
 };
-
+// update method created with lodash throttle
 var update = _.throttle(function () {
         var state = sm.states[sm.currentState],
         now = new Date(),
@@ -26,7 +27,7 @@ var update = _.throttle(function () {
         state.update.call(sm, sm, secs);
         sm.lt = now;
     }, 1000);
-
+// app loop calling the update method
 var loop = function () {
     setTimeout(loop, 100);
     update();

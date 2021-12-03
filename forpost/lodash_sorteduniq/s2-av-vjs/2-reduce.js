62793c416da72065ec15da9227c2db852dc96aa3
dustinpfister <dustin.pfister@gmail.com>
2021-12-03 10:13:17 -0500

    using object is method in reduce example

--- forpost/lodash_sorteduniq/s2-av-vjs/2-reduce.js
+++ forpost/lodash_sorteduniq/s2-av-vjs/2-reduce.js
@@ -1,10 +1,8 @@
 let sortedUniq = (arr) => {
-    let b = [];
     return arr.reduce((acc, el) => {
-        if (!b.some((c) => {
-            return c === el;
+        if (!acc.some((c) => {
+            return Object.is(c, el);
         })){
-            b.push(el);
             acc.push(el);
         }
         return acc;
@@ -14,14 +12,3 @@ let sortedUniq = (arr) => {
 let a = [1, 1, 2, 2, 3, 3, 4, 4, 4, 4],
 b = sortedUniq(a);
 console.log(b);
-
-/*
-let sortedUniq = arr => {
-return [...new Set(arr)].sort();
-};
-
-let a = [1, 1, 2, 2, 3, 3, 4, 4, 4, 4],
-b = sortedUniq(a);
-
-console.log(b); // [1,2,3,4];
-*/

40e760b74f1a6a8696f75229a40b46bbfbc5db8d
dustinpfister <dustin.pfister@gmail.com>
2021-12-03 09:11:15 -0500

    reduce example

---
+++ forpost/lodash_sorteduniq/s2-av-vjs/2-reduce.js
@@ -0,0 +1,27 @@
+let sortedUniq = (arr) => {
+    let b = [];
+    return arr.reduce((acc, el) => {
+        if (!b.some((c) => {
+            return c === el;
+        })){
+            b.push(el);
+            acc.push(el);
+        }
+        return acc;
+    }, []);
+};
+
+let a = [1, 1, 2, 2, 3, 3, 4, 4, 4, 4],
+b = sortedUniq(a);
+console.log(b);
+
+/*
+let sortedUniq = arr => {
+return [...new Set(arr)].sort();
+};
+
+let a = [1, 1, 2, 2, 3, 3, 4, 4, 4, 4],
+b = sortedUniq(a);
+
+console.log(b); // [1,2,3,4];
+*/

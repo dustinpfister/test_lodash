290b2ba6409138f770976c157d2979a63a58bb02
dustinpfister <dustin.pfister@gmail.com>
2021-12-06 13:42:21 -0500

    mix in method

--- forpost/lodash_mixin/s3-av-vjs/1-functions.js
+++ forpost/lodash_mixin/s3-av-vjs/1-functions.js
@@ -1,4 +1,14 @@
-
+// mix in method
+var mixin = function (obj, source) {
+    Object.keys(source).forEach(function (key) {
+        var value = source[key];
+        if (typeof value === 'function') {
+            obj[key] = value;
+        }
+    });
+};
+// DEMO
+// source object as a collection of utils
 var utils = {};
 utils.atan = function (string) {
     return Math.atan2(this.y, this.x) / Math.PI * 180;
@@ -6,17 +16,10 @@ utils.atan = function (string) {
 utils.dist = function (x, y) {
     return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
 };
-
-let obj = {
+// point object
+let pt = {
     x: 0,
     y: 0
 };
-
-Object.keys(utils).forEach(function (key) {
-    var value = utils[key];
-    if (typeof value === 'function') {
-        obj[key] = value;
-    }
-});
-
-console.log( obj.dist(10, 5).toFixed(2) ); // 11/18
+mixin(pt, utils);
+console.log(pt.dist(10, 5).toFixed(2)); // 11/18

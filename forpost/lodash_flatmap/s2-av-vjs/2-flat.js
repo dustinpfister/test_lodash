b92e1a78abdda65f4946435dcd5cea5b8e25bb3c
dustinpfister <dustin.pfister@gmail.com>
2021-12-09 08:30:34 -0500

    updated first vjs example

--- forpost/lodash_flatmap/s2-av-vjs/2-flat.js
+++ forpost/lodash_flatmap/s2-av-vjs/2-flat.js
@@ -2,13 +2,10 @@ let fromRed = (r) => {
     let g = 128 + Math.floor(r / 255) * 64,
     b = 255 - r;
     return [r, g, b];
-};
+}
 
 // array map, reduce, and concat is one option
-let a = [0, 255].map(fromRed);
-console.log(a);
-// [ [ 0, 128, 255 ], [ 255, 192, 0 ] ]
-let flat = a.reduce((acc, val) => acc.concat(val));
+flat = [0, 255].map(fromRed).reduce((acc, val) => acc.concat(val));
 console.log(flat);
 // [ 0, 128, 255, 255, 192, 0 ]
 

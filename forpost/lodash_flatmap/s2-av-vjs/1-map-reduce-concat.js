b7fe181d8f3959f98c2b4d02291fe0ceaa6970c0
dustinpfister <dustin.pfister@gmail.com>
2021-12-09 08:33:32 -0500

    commnets

--- forpost/lodash_flatmap/s2-av-vjs/1-map-reduce-concat.js
+++ forpost/lodash_flatmap/s2-av-vjs/1-map-reduce-concat.js
@@ -3,11 +3,11 @@ let fromRed = (r) => {
     b = 255 - r;
     return [r, g, b];
 };
-
-// array map, reduce, and concat is one option
+// Uisng array map to create an array of arrays
 let a = [0, 255].map(fromRed);
 console.log(a);
 // [ [ 0, 128, 255 ], [ 255, 192, 0 ] ]
+// now using reduce and concat methods to flatten
 let flat = a.reduce((acc, val) => acc.concat(val));
 console.log(flat);
 // [ 0, 128, 255, 255, 192, 0 ]

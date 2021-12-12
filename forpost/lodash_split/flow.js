73823b6788d61902f9ab2e32c8aeeeb961039a44
dustinpfister <dustin.pfister@gmail.com>
2021-12-12 08:51:52 -0500

    flow example now using compact

--- forpost/lodash_split/flow.js
+++ forpost/lodash_split/flow.js
@@ -5,6 +5,9 @@ var css = 'color:red;font-size:12pt;font-family:arial;';
 var toArr = _.flow([
     function (css) {
         return _.split(css, ';')
+    },
+    function (arr) {
+        return _.compact(arr);
     }
 ]);
 

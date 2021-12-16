6cc8c59dbf873a057eb286c2dd2da1f0909831e8
dustinpfister <dustin.pfister@gmail.com>
2021-12-16 12:06:52 -0500

    better 2-nth example

--- forpost/lodash_nth/s1-basics/2-nth.js
+++ forpost/lodash_nth/s1-basics/2-nth.js
@@ -1,6 +1,9 @@
 _ = require('lodash');
 
  var arr = ['fear','the','foo','man','chew'];
- 
  console.log(arr[-3]); // undefined
+ 
+ // the nth method will wrap with negative numbers
  console.log( _.nth(arr, -3) ); // 'foo'
+ // but it will not wrap values at length or higher
+  console.log( _.nth(arr, 6) ); // undefined

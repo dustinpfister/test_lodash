261f46f6ba70555ecfb8491d0648df3cb77e3351
dustinpfister <dustin.pfister@gmail.com>
2021-12-17 11:41:11 -0500

    vjs get

--- forpost/lodash_clamp/1-get.js
+++ forpost/lodash_clamp/1-get.js
@@ -1,5 +1,3 @@
-_ = require('lodash');
-
 var get = function (arr, index, def) {
     def = def === undefined ? 0 : def;
     var a = arr[index];
@@ -7,6 +5,6 @@ var get = function (arr, index, def) {
 };
 // using this get method
 var arr = [1, 2, 3];
-console.log( _.get(arr, -1, 0) ); // 0
-console.log( _.get(arr, 2, 0) );  // 3
-console.log( _.get(arr, 3, 0) );  // 0
+console.log( get(arr, -1, 0) ); // 0
+console.log( get(arr, 2, 0) );  // 3
+console.log( get(arr, 3, 0) );  // 0

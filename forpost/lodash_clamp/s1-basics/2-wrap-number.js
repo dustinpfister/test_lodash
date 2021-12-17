f3d9d143d69e2adc2f0e834170518fb1b849735f
dustinpfister <dustin.pfister@gmail.com>
2021-12-17 11:11:02 -0500

    cleaned up wrap number example

--- forpost/lodash_clamp/s1-basics/2-wrap-number.js
+++ forpost/lodash_clamp/s1-basics/2-wrap-number.js
@@ -1,23 +1,10 @@
 _ = require('lodash');
-
 // adding a _.wrapNumber method to lodash
 _.mixin({'wrapNumber': function(n, b){
     n = n === undefined ? 0 : n;
     b = b === undefined ? 1 : b;
     return (n % b + b) % b;
 }});
-// updated nth2 method that WILL WRAP
-_.mixin({'nth2': function(arr, i){
-    return arr[_.wrapNumber(i, arr.length)];
-}});
-
 // can use the wrap number method directly
 console.log(_.wrapNumber(-1, 10)); // 9
 console.log(_.wrapNumber(10, 10)); // 0
-
- // the nth2 method will wrap with negative numbers, 
- // and index values at and above array length
- var arr = ['fear','the','foo','man','chew'];
- console.log(arr[-3]); // undefined
- console.log( _.nth2(arr, -3) ); // 'foo'
- console.log( _.nth2(arr, 6) ); // 'the'

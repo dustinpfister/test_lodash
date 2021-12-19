578428e3c335a5b54c2d8556a3e2801ca8aab800
dustinpfister <dustin.pfister@gmail.com>
2021-12-19 10:08:17 -0500

    array filter example

--- forpost/lodash_filter/s2-av-vjs/1-array-filter.js
+++ forpost/lodash_filter/s2-av-vjs/1-array-filter.js
@@ -1,23 +1,6 @@
-var obj = {
-    foo: 'bar',
-    bool: false,
-    n: 42,
-    c: 7
-};
+let a = [4, -1, 7, 7, -3, -5, 1];
 
-// Vanilla js Array.filter will not just work on any object
-var numbers = [].filter.call(obj, function(val,key,obj){
-    return typeof val === 'number';
+let b = a.filter(function(val){
+    return val > 0;
 });
-console.log(numbers); // [];
- 
-// but it will work on 'array like' objects
-var numbers = [].filter.call({
-    0 : 'foo',
-    1: 'man',
-    2: 7,
-    length: 3
-}, function(val,key,obj){
-    return typeof val === 'number';
-});
-console.log(numbers); // [7]
+console.log( b ); // [4, 7, 7, 1]

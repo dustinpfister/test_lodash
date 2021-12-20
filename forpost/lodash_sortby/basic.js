482da69721f5fc5f75b27c13400d582852f9feec
dustinpfister <dustin.pfister@gmail.com>
2021-12-20 11:51:10 -0500

    basic sort by example looks good

--- forpost/lodash_sortby/basic.js
+++ forpost/lodash_sortby/basic.js
@@ -3,8 +3,13 @@ let _ = require('lodash');
 let nums = [5, 42, -5, 7, 6, 3, 52, 27, 158, -1];
 
 console.log(_.sortBy(nums));
-
 //[ -5, -1, 3, 5, 6, 7, 27, 42, 52, 158 ]
+// using a function gives the same result
+console.log(_.sortBy(nums, (n)=>{
+    return n;
+}));
+// does not mutate in place
+console.log(nums);
 
 
 let lessThanTen = _.sortBy(nums, function (n) {

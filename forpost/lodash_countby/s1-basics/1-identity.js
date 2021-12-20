3e09f41d58eaf14b99e92a550a149846841bb4f6
dustinpfister <dustin.pfister@gmail.com>
2021-12-20 15:44:10 -0500

    new identity example

--- forpost/lodash_countby/s1-basics/1-identity.js
+++ forpost/lodash_countby/s1-basics/1-identity.js
@@ -1,6 +1,12 @@
 let _ = require('lodash');
 
 let arr = [1, 2, 2, 1, 2];
-let obj = _.countBy(arr, _.identity);
-console.log(obj);
+// this also just happens to be the default
+let a = _.countBy(arr);
+console.log(a);
+// { '1': 2, '2': 3 }
+
+// using the identity method
+let b = _.countBy(arr, _.identity);
+console.log(b);
 // { '1': 2, '2': 3 }

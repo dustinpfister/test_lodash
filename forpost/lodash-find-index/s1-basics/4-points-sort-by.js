f0d82a15736f7c4f29ce2f77b5dce317ffcfef02
dustinpfister <dustin.pfister@gmail.com>
2021-12-20 12:10:30 -0500

    4-points-sort-by example done

--- forpost/lodash-find-index/s1-basics/4-points-sort-by.js
+++ forpost/lodash-find-index/s1-basics/4-points-sort-by.js
@@ -1,7 +1,13 @@
 const _ = require('lodash');
 
-// get index by points range
+// get index by name
+const getIndexByName = (users, name) => {
+    return _.findIndex(users, (el) => {
+        return el === name;
+    });
+};
 
+// get index by points range
 const getIndexByPointsRange = (users, pointsMin, pointsMax) => {
     return _.findIndex(users, (obj) => {
         return obj.points >= pointsMin && obj.points <= pointsMax;
@@ -15,6 +21,15 @@ let users = [
     { name: 'Bill', points: 935}
 ];
 
-
+// if I just get by point range it will just get the first element
+// but maybe not the best of the array is not sorted first.
+console.log( getIndexByPointsRange(users, 500, 1000) ); // 1
+// So then there is creating a new sorted array with lodash chain, sort by, and reverse
 let sortedUsers = _.chain(users).sortBy((obj)=>{ return obj.points}).reverse().value();
-console.log(sortedUsers);
+// If I use the getIndexByPoints method now with the sorted array I get an index
+// that is a higher value, but the index value is relative to the new sorted array rather than
+// the original source array users
+let si = getIndexByPointsRange(sortedUsers, 500, 1000);
+console.log(si) // 0
+// But I can use the find index method again with the sorted object to get that index
+console.log( _.findIndex(users, sortedUsers[si]) ); // 3
--- forpost/lodash-find-index/todo.md
+++ forpost/lodash-find-index/todo.md
@@ -2,7 +2,7 @@
 
 ### () - expand basic section
 * (done) 3-points example
-* 4-points-sort-by example that is same as points but sorting the collection
+* (done) 4-points-sort-by example that is same as points but sorting the collection
 * 5-points-reduce example using lodash reduce to get all index values for a given point range
 
 ### ( done 12/20/2021 ) - expand basic section

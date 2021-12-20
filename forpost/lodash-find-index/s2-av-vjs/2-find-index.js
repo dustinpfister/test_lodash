56ebe78850fffef660faef893707dbd11ccbd51e
dustinpfister <dustin.pfister@gmail.com>
2021-12-20 08:58:45 -0500

    2-find-index example

--- forpost/lodash-find-index/s2-av-vjs/2-find-index.js
+++ forpost/lodash-find-index/s2-av-vjs/2-find-index.js
@@ -1,15 +1,10 @@
-var objs = [{
-        n: 12
-    }, {
-        n: 42
-    }, {
-        n: 7
-    }
+let objs = [
+    { n: 12 },
+    { n: 42 }, 
+    { n: 7 }
 ];
- 
 // Array.findIndex works given the proper method
-console.log( objs.findIndex(function(el){
- 
+let i = objs.findIndex(function(el){
     return el.n === 42;
- 
-}) ); // 1
+});
+console.log( i ); // 1

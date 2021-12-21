165d78ef481f27b177161e50aa1216dcb6b1b5f3
dustinpfister <dustin.pfister@gmail.com>
2021-12-21 11:48:51 -0500

    comments

--- forpost/lodash_concat/s1-basics/4-chain-sum.js
+++ forpost/lodash_concat/s1-basics/4-chain-sum.js
@@ -1,16 +1,14 @@
 let _ = require('lodash');
 
-// array a as an array of arrays
+// array a
 let a = [
   [0,[1,0]],
   [[0,1],1]
 ];
-// array b as a array of numbers
+// array b
 let b = [1,[ 0,1 ]];
-
+// number c
 let c = 1;
-
-
+// using concat flatten deep, and lodash sum methods
 let d = _.chain(a).concat(b, c).flattenDeep().sum().value();
 console.log(d);
-// 6

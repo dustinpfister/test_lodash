2da77c9c266d9c34dc629b38822aa155fb86e656
dustinpfister <dustin.pfister@gmail.com>
2021-12-22 09:19:35 -0500

    better 1-forin example

--- forpost/lodash_forin/s2-av-vjs/1-forin.js
+++ forpost/lodash_forin/s2-av-vjs/1-forin.js
@@ -1,5 +1,12 @@
-let a = new A();
- 
+// Simple Constructor and Prototype
+let A = function () {
+    this.b = 42
+};
+A.prototype.c = 7;
+// using the for in loop
+let a = new A(),
+str = '';
 for (let prop in a) {
-  console.log(prop + ' : ' +a[prop]);
+  str += prop + ':' +a[prop] + ';';
 }
+console.log(str); // b:42;c:7;

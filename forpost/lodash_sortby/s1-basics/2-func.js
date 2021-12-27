d299f143bb51707eeb287f96c74586dbfa7a7895
dustinpfister <dustin.pfister@gmail.com>
2021-12-27 14:39:33 -0500

    nums example

---
+++ forpost/lodash_sortby/s1-basics/2-func.js
@@ -0,0 +1,10 @@
+let _ = require('lodash');
+
+let nums = [5, 42, -5, 7, 6, 3, 52, 27, 158, -1];
+let lessThanTen = _.sortBy(nums, function (n) {
+  return n < 10;
+});
+console.log(lessThanTen);
+// [ 42, 52, 27, 158, 5, -5, 7, 6, 3, -1 ]
+
+

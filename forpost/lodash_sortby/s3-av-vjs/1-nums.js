7ac439fadd8abf27a328e228d9df44edf98330dd
dustinpfister <dustin.pfister@gmail.com>
2021-12-28 11:11:08 -0500

    fixed comments

--- forpost/lodash_sortby/s3-av-vjs/1-nums.js
+++ forpost/lodash_sortby/s3-av-vjs/1-nums.js
@@ -1,8 +1,10 @@
 let nums = [5, 42, -5, 7, 6, 3, 52, 27, 158, -1];
 let a = nums.sort();
+// by default the sort method will not sort values by number
+// value
 console.log(a);
-//[ -5, -1, 3, 5, 6, 7, 27, 42, 52, 158 ]
+// [ -1, -5, 158, 27, 3, 42, 5, 52, 6, 7 ]
 // this will mutate in place
 console.log(nums);
-//[ -5, -1, 3, 5, 6, 7, 27, 42, 52, 158 ]
+// [ -1, -5, 158, 27, 3, 42, 5, 52, 6, 7 ]
 
--- forpost/lodash_sortby/s3-av-vjs/2-func.js
+++ forpost/lodash_sortby/s3-av-vjs/2-func.js
@@ -1,5 +1,6 @@
 // using array sort with a function
 let nums = [5, 42, -5, 7, 6, 3, 52, 27, 158, -1];
+
 let a = nums.sort(function (a, b) {
     if (a > b) {
         return -1;
@@ -12,6 +13,7 @@ let a = nums.sort(function (a, b) {
 console.log(a);
 // [ 158, 52, 42, 27, 7, 6, 5, 3, -1, -5 ]
 
+ 
 // compared to sort by itself
 console.log(nums.sort());
 // [ -1, -5, 158, 27, 3, 42, 5, 52, 6, 7 ]

77b56aca7cc1dec3aa2d1f81661ff691016ab2df
dustinpfister <dustin.pfister@gmail.com>
2021-12-28 10:50:33 -0500

    better 2-func example for sort by

--- forpost/lodash_sortby/s1-basics/2-func.js
+++ forpost/lodash_sortby/s1-basics/2-func.js
@@ -1,5 +1,7 @@
 let _ = require('lodash');
 
+// using sortBy method by giving a function with
+// custom sort logic
 let nums = [5, 42, -5, 7, 6, 3, 52, 27, 158, -1];
 let a = _.sortBy(nums, function (n) {
     if (n > 0) {
@@ -8,5 +10,13 @@ let a = _.sortBy(nums, function (n) {
     return Math.abs(n);
 });
 console.log(a);
-// [ 42, 52, 27, 158, 5, -5, 7, 6, 3, -1 ]
+// [ 158, 52, 42, 27, 7, 6, 5, 3, -1, -5 ]
+
+// compare to just using _.sortBy with the 
+// source array itself, also the sortBy method
+// will not mutate the source array in place
+console.log(_.sortBy(nums));
+// [ -5, -1, 3, 5, 6, 7, 27, 42, 52, 158 ]
+console.log(nums);
+// [ 5, 42, -5, 7, 6, 3, 52, 27, 158, -1 ]
 

aeb587e75d07d775541bc7a9764bf83a931a6e4d
dustinpfister <dustin.pfister@gmail.com>
2021-12-28 11:04:32 -0500

    vjs section looks good

--- forpost/lodash_sortby/s3-av-vjs/2-func.js
+++ forpost/lodash_sortby/s3-av-vjs/2-func.js
@@ -1,14 +1,22 @@
-let _ = require('lodash');
-
+// using array sort with a function
 let nums = [5, 42, -5, 7, 6, 3, 52, 27, 158, -1];
-let lessThanTen = nums.sort(nums, function (a, b) {
-	
-	if(a < 10 && !(b < 10) ){
-		return 1;
-	}
-	return 0;
+let a = nums.sort(function (a, b) {
+    if (a > b) {
+        return -1;
+    }
+    if (a < b) {
+        return 1;
+    }
+    return 0;
 });
-console.log(lessThanTen);
-// [ 42, 52, 27, 158, 5, -5, 7, 6, 3, -1 ]
+console.log(a);
+// [ 158, 52, 42, 27, 7, 6, 5, 3, -1, -5 ]
+
+// compared to sort by itself
+console.log(nums.sort());
+// [ -1, -5, 158, 27, 3, 42, 5, 52, 6, 7 ]
+console.log(nums);
+// [ -1, -5, 158, 27, 3, 42, 5, 52, 6, 7 ]
+
 
 
--- forpost/lodash_sortby/todo.md
+++ forpost/lodash_sortby/todo.md
@@ -1,8 +1,9 @@
 # lodash sort by todo
 
-## () - update 2-func example in basics section
+## ( done 12/28/2021 ) - update 2-func example in basics section
+* (done) fix the 2-func example so that it works better as an example of how sortby works
 
-## () - new av-vjs section
+## ( done 12/28/2021 ) - new av-vjs section
 * (done) 1-nums example using array sort method
-* 2-func example using array sort
+* (done) 2-func example using array sort
 

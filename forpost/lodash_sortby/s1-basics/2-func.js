d89f2d421cfa7c2fd075ee001c7c912fb74ba15d
dustinpfister <dustin.pfister@gmail.com>
2021-12-28 10:47:23 -0500

    better 2-func example

--- forpost/lodash_sortby/s1-basics/2-func.js
+++ forpost/lodash_sortby/s1-basics/2-func.js
@@ -1,10 +1,12 @@
 let _ = require('lodash');
 
 let nums = [5, 42, -5, 7, 6, 3, 52, 27, 158, -1];
-let lessThanTen = _.sortBy(nums, function (n) {
-  return n < 10;
+let a = _.sortBy(nums, function (n) {
+    if (n > 0) {
+        return Math.abs(n) * -1;
+    }
+    return Math.abs(n);
 });
-console.log(lessThanTen);
+console.log(a);
 // [ 42, 52, 27, 158, 5, -5, 7, 6, 3, -1 ]
-
 

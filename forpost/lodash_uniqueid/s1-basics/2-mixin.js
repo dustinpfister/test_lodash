c7b9782535867d5bf9ffc7053acc3aa12bd6c01a
dustinpfister <dustin.pfister@gmail.com>
2021-12-29 11:14:35 -0500

    good mixin example

--- forpost/lodash_uniqueid/s1-basics/2-mixin.js
+++ forpost/lodash_uniqueid/s1-basics/2-mixin.js
@@ -1,7 +1,8 @@
 let _ = require('lodash');
 
 _.mixin({
-    myUID: () => {
+    // create a myUID
+    getUID: () => {
         let count = parseInt(_.uniqueId()),
         n = count * 16,
         b = Math.floor(Math.pow(1.57, 42)),
@@ -9,9 +10,36 @@ _.mixin({
         hex = c.toString(16),
         prefix = 'uh';
         return prefix + '_' + hex;
+    },
+    // break down
+    breakUID: (str) => {
+        let parts = str.split('_'),
+        b =  Math.floor(Math.pow(1.57, 42)),
+        hex = parts[1],
+        c = parseInt(hex, 16);
+        return {
+            count: (c-b) / 16,
+            n: c - b,
+            b: b,
+            c: c,
+            hex: hex,
+            prefix: parts[0]
+        };
     }
 });
 
-console.log(_.myUID()); // uh_a122257
-console.log(_.myUID()); // uh_a122267
-console.log(_.myUID()); // uh_a122277
+console.log(_.getUID()); // uh_a122257
+console.log(_.getUID()); // uh_a122267
+console.log(_.getUID()); // uh_a122277
+
+console.log( _.breakUID('uh_a122277') );
+/*
+{
+    count: 3,
+    n: 48,
+    b: 168960583,
+    c: 168960631,
+    hex: 'a122277',
+    prefix: 'uh'
+}
+*/

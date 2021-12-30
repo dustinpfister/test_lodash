d8659052e4bb26effa9d9859b5d0ac0cdaecd70f
dustinpfister <dustin.pfister@gmail.com>
2021-12-30 10:38:23 -0500

    comments

--- forpost/lodash_forown/s2-av-vjs/1-forin.js
+++ forpost/lodash_forown/s2-av-vjs/1-forin.js
@@ -3,11 +3,8 @@ let Unit = function (obj) {
     this.x = obj.x || 0;
     this.y = obj.y || 0;
 };
- 
-// size value in the prototype
 Unit.prototype.size = 64;
- 
-// new Unit
+// DEMO using the for in loop and Object.hasOwnProperty
 let u = new Unit({ x : 12,y : 5});
 let key, val;
 for(key in u){

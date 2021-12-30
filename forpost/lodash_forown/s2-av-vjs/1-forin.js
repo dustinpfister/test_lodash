5004db0ff710fd38bea448d44bc8db4e5edfa53f
dustinpfister <dustin.pfister@gmail.com>
2021-12-30 10:37:21 -0500

    1-forin example

--- forpost/lodash_forown/s2-av-vjs/1-forin.js
+++ forpost/lodash_forown/s2-av-vjs/1-forin.js
@@ -1,29 +1,20 @@
-var Unit = function (obj) {
- 
+let Unit = function (obj) {
     obj = obj || {};
     this.x = obj.x || 0;
     this.y = obj.y || 0;
- 
 };
  
 // size value in the prototype
 Unit.prototype.size = 64;
  
 // new Unit
-var u = new Unit({
-        x : 12,
-        y : 5
-    });
-
-var key,val;
+let u = new Unit({ x : 12,y : 5});
+let key, val;
 for(key in u){
- 
-    // using Object.hasOwnProperty to find if a property is directly on an object
+    // using Object.hasOwnProperty to find if a property is and
+    // own property or not
     if(u.hasOwnProperty(key)){
- 
         val = u[key];
         console.log(key + ' : '+val);
- 
     }
- 
 }

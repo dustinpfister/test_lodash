95a741e08f50ce1a362f2fbd16a0b5ae8bf79dc5
dustinpfister <dustin.pfister@gmail.com>
2022-01-07 12:00:35 -0500

    basic example looks great

--- forpost/lodash_tap/s1-basics/1-basic.js
+++ forpost/lodash_tap/s1-basics/1-basic.js
@@ -1,9 +1,15 @@
 let _ = require('lodash');
 
 
-// basic example using an array of numbers
-let a = [4, -1, 7, 7, -3, -5, 1];
-let b = _.filter(a, function(val){
-    return val > 0;
-});
-console.log( b ); // [4, 7, 7, 1]
+let a = [1,2,3,4,5]
+// using tap with lodash chain, and map
+let b = _.chain(a)
+.map((n)=>{
+    return Math.pow(n, 2);
+})
+.tap((a)=>{
+    a[3] = 0;
+})
+.value();
+console.log(b);
+//[ 1, 4, 9, 0, 25 ]

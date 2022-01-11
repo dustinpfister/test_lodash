e1e1dc79052b0b36189c640f0c93c420221f7554
dustinpfister <dustin.pfister@gmail.com>
2022-01-11 11:39:22 -0500

    clean up of matrix example

--- forpost/lodash-chunk/s2-ex-matrix/1-matrix.js
+++ forpost/lodash-chunk/s2-ex-matrix/1-matrix.js
@@ -2,8 +2,7 @@
 let _ = require('lodash');
  
 // matrix data example
-data = {
- 
+let data = {
     w : 10,
     colors : ['grey', 'blue','red'],
     px : [
@@ -12,30 +11,20 @@ data = {
         1,1,1,1,2,2,2,2,2,2,
         0,0,0,0,0,0,0,0,0,0,
         2,2,2,2,2,2,2,2,2,2],
- 
     // my toMatrix method using _.chunk
     toMatrix : function(){
- 
         return _.chunk(this.px,this.w);
- 
     }
- 
 };
  
 // draw it out like this
-
 var html = '';
 data.toMatrix().forEach(function (line) {
- 
     line.forEach(function (px) {
- 
         html += '<span style=\"color: '+data.colors[px]+';\">X<\/span>';
- 
     });
     html += '<br>';
- 
 });
-
 console.log(html);
 /*
 <span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><br><span style="color: blue;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: blue;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><br><span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: blue;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><br><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><span style="color: grey;">X</span><br><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><span style="color: red;">X</span><br>

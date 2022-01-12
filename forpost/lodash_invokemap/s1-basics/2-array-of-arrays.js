176ed0fec264cd8ecee166901feeb7192c0843c1
dustinpfister <dustin.pfister@gmail.com>
2022-01-12 12:34:49 -0500

    array of arrays clean up

--- forpost/lodash_invokemap/s1-basics/2-array-of-arrays.js
+++ forpost/lodash_invokemap/s1-basics/2-array-of-arrays.js
@@ -1,22 +1,10 @@
 let _ = require('lodash');
 
-let arr = [
-    [7, 56, 3, 3, 0, 12],
-    [6, 5, 4],
-    [5, 5, 5, 1]
-];
-
-let sorter = function (a, b) {
-    if (a < b) {
-        return -1;
-    }
-    if (a > b) {
-        return 1;
-    }
+let arr = [ [7, 56, 3, 3, 0, 12], [6, 5, 4], [5, 5, 5, 1] ];
+let r = _.invokeMap(arr, 'sort', function (a, b) {
+    if (a < b) { return -1; }
+    if (a > b) { return 1; }
     return 0;
-};
-
-let r = _.invokeMap(arr, 'sort', sorter);
-
+});
 console.log(r);
 // [ [ 0, 3, 3, 7, 12, 56 ], [ 4, 5, 6 ], [ 1, 5, 5, 5 ] ]

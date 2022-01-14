e3c8d8f7442df2d38e23b0a7b2f16b381da1d25b
dustinpfister <dustin.pfister@gmail.com>
2022-01-14 10:39:11 -0500

    basic example looks good

--- forpost/lodash_collection/s1-basics/1-basic.js
+++ forpost/lodash_collection/s1-basics/1-basic.js
@@ -1,13 +1,38 @@
 let _ = require('lodash');
 
-let color = {
-    a: 255,
-    r: 128,
-    b: 0,
-    g: 64
+// an Array
+let a = [ 1, 2, 3];
+ 
+// an Object that is formated like an Array
+let b = {
+    0: 1,
+    1: 2,
+    2: 3
+};
+Object.defineProperty(b, 'length', {
+    value: 3
+});
+ 
+// An Object with named rather than numbered keys and no length property
+let c = {
+    zero: 1,
+    one: 2,
+    two: 3
 };
 
-let cArr = _.at(color, ['r','g','b', 'a']);
-
-console.log(cArr);
-// [ 128, 64, 0, 255 ]
+// a collection method like lodash forEach, will work with all of these
+let func = (item, key, obj) => {
+    console.log(key + ' : ' + item + ' : ' + Object.keys(obj).length );
+};
+_.forEach(a, func);
+// 0: 1: 3 
+// 1: 2: 3 
+// 2: 3: 3 
+_.forEach(b, func);
+// 0: 1: 3 
+// 1: 2: 3 
+// 2: 3: 3 
+_.forEach(c, func);
+// zero: 1: 3 
+// one: 2: 3 
+// two: 3: 3

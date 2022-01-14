5ba6576bd63fb0c23a1e842367fe72af8775e0a0
dustinpfister <dustin.pfister@gmail.com>
2022-01-14 07:59:53 -0500

    1-filter now uses Object.is

--- forpost/lodash_difference/s3-av-vjs/1-filter.js
+++ forpost/lodash_difference/s3-av-vjs/1-filter.js
@@ -3,7 +3,7 @@ kill = [4, 8];
 // Using Array.filter and Array.some
 let result = data.filter(function (n) {
     return !kill.some(function (kn) {
-        return kn === n;
+        return Object.is(kn, n);
     });
 });
 console.log(result); // [2, 6, 9]

ebf11de38cf0dfa4fd234402008958c05f09932c
dustinpfister <dustin.pfister@gmail.com>
2022-01-19 09:12:53 -0500

    no mutate example

---
+++ forpost/lodash_first/s1-basics/2-no-mutate.js
@@ -0,0 +1,10 @@
+let _ = require('lodash');
+
+let arr = ['foo','bar', 'baz'];
+// the lodash first method will not mutate in place
+console.log( _.first(arr) ); // 'foo'
+console.log( arr ); // ['foo','bar', 'baz'];
+ 
+// other methods like pullAt will mutate in place
+console.log(_.pullAt(arr, 0));
+console.log( arr ); // ['foo','bar', 'baz'];

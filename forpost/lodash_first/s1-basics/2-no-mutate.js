f195687fa4fcf8e134dff03125a80b50e88c4a6a
dustinpfister <dustin.pfister@gmail.com>
2022-01-19 09:26:45 -0500

    log primative rather than array in 2-no-mutate in lodash first basic section folder

--- forpost/lodash_first/s1-basics/2-no-mutate.js
+++ forpost/lodash_first/s1-basics/2-no-mutate.js
@@ -6,5 +6,5 @@ console.log( _.first(arr) ); // 'foo'
 console.log( arr ); // ['foo','bar', 'baz'];
  
 // other methods like pullAt will mutate in place
-console.log(_.pullAt(arr, 0));
+console.log(_.pullAt(arr, 0)[0]); // 'foo'
 console.log( arr ); // ['foo','bar', 'baz'];

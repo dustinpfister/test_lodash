56c395081e1d4832eadc0e8574dea969492b94cc
dustinpfister <dustin.pfister@gmail.com>
2022-01-23 13:55:08 -0500

    broke up basic example to reflext state of post sections

--- forpost/lodash_round/s1-basics/1-basic.js
+++ forpost/lodash_round/s1-basics/1-basic.js
@@ -2,12 +2,3 @@ let _ = require('lodash');
 
 // so _.round works just fine for just rounding a number...
 console.log(_.round(Math.PI)); // 3
-
-// But so does Math.round.
-console.log(Math.round(Math.PI)); // 3
-
-// however _.round can make use of a precision argument
-console.log(_.round(Math.PI,2)); // 3.14
-
-// where is Math.round out of the box does not
-console.log(Math.round(Math.PI,2)); // 3
---
+++ forpost/lodash_round/s1-basics/2-per.js
@@ -0,0 +1,4 @@
+let _ = require('lodash');
+
+//  _.round has a precision argument
+console.log(_.round(Math.PI,2)); // 3.14

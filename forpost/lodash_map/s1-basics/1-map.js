07f9cbb0b461613e281b9bd07ec4f59306f35f34
dustinpfister <dustin.pfister@gmail.com>
2022-01-24 09:12:20 -0500

    1-map

--- forpost/lodash_map/s1-basics/1-map.js
+++ forpost/lodash_map/s1-basics/1-map.js
@@ -1,12 +1,17 @@
 let _ = require('lodash');
 
 
-var arr = [1,2,3,4,5];
-arr = _.map(arr, function(el){
- 
+let source = [1,2,3,4,5];
+// create a new array from the source array by preforming
+// an action with the given function for each element in the
+// source array using lodash map
+let b = _.map(source, function(el){
     return el * 10;
- 
 });
- 
-console.log(arr);
+// lodash map creates the new array and WILL NOT mutate the 
+// source array in place when doing so
+console.log(b);
 // [10,20,30,40,50]
+console.log(source);
+// [ 1, 2, 3, 4, 5 ]
+

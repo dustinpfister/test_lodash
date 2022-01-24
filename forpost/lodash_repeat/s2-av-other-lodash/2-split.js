e87e02bf03e151699cdd500876b62730e17c42c3
dustinpfister <dustin.pfister@gmail.com>
2022-01-24 11:07:00 -0500

    cleaner lodash split example

--- forpost/lodash_repeat/s2-av-other-lodash/2-split.js
+++ forpost/lodash_repeat/s2-av-other-lodash/2-split.js
@@ -1,6 +1,5 @@
 let _ = require('lodash');
 
-let str = _.repeat('1,', 5);
-let arr = _.split(str, ',');
-arr.pop();
+let str = _.repeat('1', 5);
+let arr = _.split(str, '');
 console.log(arr); // [ '1', '1', '1', '1', '1' ]

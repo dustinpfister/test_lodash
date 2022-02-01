0b5b39fb4821dbcd4a52d04a9289eefbd457857d
dustinpfister <dustin.pfister@gmail.com>
2022-02-01 15:33:13 -0500

    1-create example

--- forpost/lodash_create/s1-basics/1-create.js
+++ forpost/lodash_create/s1-basics/1-create.js
@@ -1,18 +1,14 @@
 let _ = require('lodash');
 
 let methods = {
-
     move: function (x, y) {
-
         this.x += x;
         this.y += y;
-
     }
-
 };
 
-let obj = _.create(methods, {x: 5,y: 15});
+let obj = _.create(methods, {x: 5, y: 15} );
 
-obj.move(5, 5);
+obj.move(2, 5);
 
-console.log(obj.x, obj.y); // 10 20
+console.log(obj); // { x: 7, y: 20 }

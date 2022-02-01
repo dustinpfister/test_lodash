0a5966f9d03589bf4a2e89ea98de8b3523f926bf
dustinpfister <dustin.pfister@gmail.com>
2022-02-01 14:47:06 -0500

    todo

--- forpost/lodash_create/s2-av-vjs/1-object-create.js
+++ forpost/lodash_create/s2-av-vjs/1-object-create.js
@@ -1,17 +1,13 @@
 let _ = require('lodash');
 
 let methods = {
-
     move: function (x, y) {
-
         this.x += x;
         this.y += y;
-
     }
-
 };
 
-let obj = Object.create(methods, {x: {value:5,writable: true},y: {value:15,writable: true}});
+let obj = Object.create(methods, {x: {value:5,writable: true}, y: {value:15,writable: true}});
 
 obj.move(5, 5);
 
---
+++ forpost/todo.md
@@ -0,0 +1,8 @@
+# lodash create todo list
+
+## () - expand basics section
+
+## () - expand av-vjs section
+* 2-constructor
+* 3-object-get-prototype-of
+

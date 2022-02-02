b30883cdc7840c96ea1117617e9383d815f9e08e
dustinpfister <dustin.pfister@gmail.com>
2022-02-02 10:33:22 -0500

    better 1-func-bind example

--- forpost/lodash_bind/s2-av-vjs/1-func-bind.js
+++ forpost/lodash_bind/s2-av-vjs/1-func-bind.js
@@ -4,16 +4,14 @@ let obj = {
     dx: 5,
     dy: 7
 };
-
-var step = function () {
+// creating a step function with a function
+// expression, NOT an arrow function
+const step = function () {
     this.x += this.dx;
     this.y += this.dy;
 };
-
-// create an obj.step with Function.prototype.bind
-obj.step = step.bind(obj);
-
+// create a stepObj with Function.prototype.bind
+let stepObj = step.bind(obj);
 // I can now call step, and this will refer to obj
-obj.step();
-
+stepObj();
 console.log(obj.x, obj.y); // 5,7
